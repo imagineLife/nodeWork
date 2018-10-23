25fffbbb3e14a20bfc5fa5cbe443bc147c8b07ef
Eric Laursen <ericjlaursen@gmail.com>
2018-10-23 06:31:45 -0400

    Begins framework for users crud handling.

--- 17.UsersHandlers/lib/handlers.js
+++ 17.UsersHandlers/lib/handlers.js
@@ -7,8 +7,48 @@
 
 
 
-//hanlders
+//handlers
 let routeHandlers = {}
+
+//USERS handler
+//FIGURES OUT wthe req method, & passes it to sub-handlers
+routeHandlers.users = (data, callback) => {
+	const acceptableMethods('post','get','put','delete');
+
+	/*
+		if the method from the Front-End matches an acceptable method,
+		run it. use a NEW SET OF METHODS 'doUser'.
+		ELSE return 405
+	*/
+	if(acceptableMethods.indexOf(data.method) > -1){
+		routeHandlers.doUser[data.method](data,callback);
+	}else{
+		callback(405)
+	}
+}
+
+//deals with users CRUD methods
+routeHandlers.doUsers = {}
+
+//Users POST
+routeHandlers.doUsers.post = (data,callback){
+
+}
+
+//Users PUT
+routeHandlers.doUsers.put = (data,callback){
+	
+}
+
+//Users GET
+routeHandlers.doUsers.get = (data,callback){
+	
+}
+
+//Users DELETE
+routeHandlers.doUsers.delete = (data,callback){
+	
+}
 
 //ping handler
 routeHandlers.ping = (data, callback) => {
