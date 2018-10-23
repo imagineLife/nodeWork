77bce806faaafce8a1be05c918ec13747aa9afb2
Eric Laursen <ericjlaursen@gmail.com>
2018-10-23 12:28:55 -0400

    Begins hash helper function

---
+++ 17.UsersHandlers/lib/helpers.js
@@ -0,0 +1,29 @@
+/*
+	Library of 'helpers' for tasks...
+	password-hashing
+*/
+
+
+//Dependencies
+const crypto = require('crypto');
+const config = require('./config')
+
+//container of helpers
+let helpers  = {};
+
+//HASH PASSWORD takes a string
+//we are using SHA256 as the hash algorithm
+//using crypto library that comes with node
+// https://nodejs.org/api/crypto.html#crypto_crypto
+
+helpers.hash = function(str){
+	if(typeof(str) == 'string' && str.length > 0){
+		
+		//uses a hashingSecret!! from dependency config  file
+		let hashed = cryptop.createHmac('sha256', config.hashingSecret).update(str).digest('hex');
+		return hashed;
+
+	}else{
+		return false
+	}
+}
