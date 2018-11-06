2678f2a96593afd5621884de9e8804ccdcfe9721
Eric Laursen <ericjlaursen@gmail.com>
2018-11-06 14:54:52 -0500

    adds a MaxChecks key to the configs.

--- 28.checks/lib/config.js
+++ 28.checks/lib/config.js
@@ -23,8 +23,8 @@ envs.staging = {
 	httpPort: 3000,
 	httpsPort: 3001,
 	friendlyEnvName: 'staging',
-	hashingSecret: 'thisIsASecret'
-
+	hashingSecret: 'thisIsASecret',
+	'maxChecks': 5
 };
 
 //production environment
@@ -32,7 +32,8 @@ envs.prod = {
 	httpPort: 5000,
 	httpsPort: 5001,
 	friendlyEnvName: 'production',
-	hashingSecret: 'thisIsASecret'
+	hashingSecret: 'thisIsASecret',
+	'maxChecks': 5
 };
 
 //Determines which env to export 
