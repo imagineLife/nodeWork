887f1f4d6f1286c7307bf811aabf18bb4f888a25
Eric Laursen <ericjlaursen@gmail.com>
2018-11-25 11:41:18 -0500

    Loads with dependencies

---
+++ 34.BGWorkers/lib/workers.js
@@ -0,0 +1,14 @@
+/*
+
+	Web-Worker related tasks
+
+*/
+
+//Dependencies
+const path = require('path');
+const fs = require('fs');
+const dataLib = require('./data');
+const https = require('https');
+const http = require('http');
+const helpersLib = require('./helpers');
+const url = require('url');
