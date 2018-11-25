719c4a6d43fa6c9170b18c21ac6e9099124bcfab
Eric Laursen <ericjlaursen@gmail.com>
2018-11-25 10:55:05 -0500

    demo connecting to a twilio 3rd party api

---
+++ 33.ConnectToTwilio/index.js
@@ -0,0 +1,126 @@
+//Dependency
+const http = require('http');
+const https = require('https');
+const url = require('url');
+const stringDecoder = require('string_decoder').StringDecoder;
+const envConfig = require('./lib/config');
+const fs = require('fs');
+const routeHandlers = require('./lib/handlers');
+const helpers = require('./lib/helpers')
+
+
+helpers.sendTwilioSms('8603194410','MicCheck', (err) => {
+	console.log('err?')
+	console.log(err)
+})
+
+//instantiating http server
+const httpServer = http.createServer((req, res) => {
+	sharedServer(req,res)
+})
+
+//create httpsServerOptions
+//read in key & cert from https file directory
+const httpsServerOptions = {
+	'key' : fs.readFileSync('./https/key.pem'),
+	'cert' : fs.readFileSync('./https/cert.pem')
+}
+
+//start https server
+const httpsServer = https.createServer(httpsServerOptions, (req, res) => {
+	sharedServer(req,res)
+})
+
+//Start the server, listen on port 3000
+httpServer.listen(envConfig.httpPort, () => console.log(`Server is listening on port ${envConfig.httpPort} in environment ${envConfig.friendlyEnvName} mode!!`))
+
+httpsServer.listen(envConfig.httpsPort, () => console.log(`Server is listening on port ${envConfig.httpsPort} in environment ${envConfig.friendlyEnvName} mode!!`))
+
+const myRouter = {
+	'ping': routeHandlers.ping,
+	'users': routeHandlers.users,
+	'tokens' : routeHandlers.tokens,
+	'checks': routeHandlers.checks
+}
+
+//Sharing logic to create http & https servers
+const sharedServer = (req, res) => {
+
+	//get & parse the url
+	const parsedUrl = url.parse(req.url,true);
+
+	//get the 'path' name from the url, trim the pathText
+	const pathText = parsedUrl.pathname;
+	const trimmedPathTxt = pathText.replace(/^\/+|\/+$/g,'')
+
+	//Get the query String as an Object
+	/* when a url is sent with query strings, the querys will get put in the queryString*/
+	const queryString = parsedUrl.query;
+	
+	//get http method that was used. its in the req object
+	const reqMethod = req.method.toLowerCase()
+
+	//get requested headers
+	const hdrs = req.headers;
+
+	//get the payload, if is one
+	//include the encoding, utf-8
+	const decoder = new stringDecoder('utf-8');
+
+	//as new data comes in, it gets added to this var
+	let curIncomingString = ''
+
+	//when request objects emits event data, pass the data to a callback
+	//append the new incoming data to the curIncomingString with the decoder
+	req.on('data', data => {
+		// console.log('incoming data')
+		// console.log(data)
+		curIncomingString += decoder.write(data)
+	})
+
+	//when request object emits the end event
+	//end the decoder...
+	req.on('end', () => {
+
+		curIncomingString += decoder.end();
+
+		//choose the handler this request should go to
+		let chosenHandler = typeof(myRouter[trimmedPathTxt]) !== 'undefined' ? myRouter[trimmedPathTxt] : routeHandlers.notFound;
+
+		// object to send to the handler
+		let dataToReturn = {
+			trimmedPath: trimmedPathTxt,
+			queryStrObj: queryString,
+			method: reqMethod,
+			headers: hdrs,
+			//PARSING the paload data with helpers method
+			payload: helpers.parseJsonToObject(curIncomingString)
+		}
+
+		chosenHandler(dataToReturn, (statusCode, payload) =>{
+			// console.log('chosenHandler dataToReturn, statusCode & payload')
+			// console.log(dataToReturn)
+			// console.log(statusCode)
+			// console.log(payload)
+
+			//defaults if none given
+			statusCode = typeof(statusCode) === 'number' ? statusCode : 200;
+			payload = typeof(payload) === 'object' ? payload : {};
+
+			const payloadStr = JSON.stringify(payload)
+
+			//writeHead comes on a response object, writitng the status code to the head
+			res.setHeader('Content-Type', 'application/json');
+			res.writeHead(statusCode);
+			res.end(payloadStr);
+
+			//'log' the request path
+			// console.log('returning statusCode & payloadStr')
+			// console.log(statusCode)
+			// console.log(payloadStr)
+
+		})
+
+	})
+
+}
---
+++ 33.ConnectToTwilio/lib/helpers.js
@@ -0,0 +1,147 @@
+/*
+	Library of 'helpers' for tasks...
+	password-hashing
+*/
+
+
+//Dependencies
+const crypto = require('crypto');
+const config = require('./config');
+const https = require('https');
+const queryString = require('querystring');
+
+//container of helpers
+let helpers  = {};
+
+//HASH PASSWORD takes a string
+//we are using SHA256 as the hash algorithm
+//using crypto library that comes with node
+// https://nodejs.org/api/crypto.html#crypto_crypto
+
+helpers.hash = function(str){
+	if(typeof(str) == 'string' && str.length > 0){
+		
+		//uses a hashingSecret!! from dependency config  file
+		let hashed = crypto.createHmac('sha256', config.hashingSecret).update(str).digest('hex');
+		return hashed;
+
+	}else{
+		return false
+	}
+}
+
+
+//parses a json STRING to an object in all cases without throwing erro
+helpers.parseJsonToObject = function(str){
+	console.log('parsing str')
+	console.log(str)
+	try{
+		const thisObj = JSON.parse(str);
+		return thisObj;
+	}catch(e){
+		console.log('ERROR parsing to str')
+		console.log(e)
+		return {}
+	}
+}
+
+//create a str of RANDOM alphaNum chars of a length param
+helpers.createRandomString = (strLength) => {
+	strLength = typeof(strLength) == 'number' && strLength > 0 ? strLength : false;
+
+	if(strLength){
+
+		//define all POSSIBLE cars that COULD go into str
+		const possibleChars = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+		//start the rndm str
+		let str = '';
+
+		//make the str
+		for(let i = 1; i < strLength; i++){
+			//get random char
+			const rndmChr =  possibleChars.charAt(Math.floor(Math.random() * possibleChars.length))
+
+			//append char to str
+			str += rndmChr;
+		}
+
+		return str
+
+	}else{
+		return false
+	}
+}
+
+
+//send SMS via Twilio
+helpers.sendTwilioSms = (phoneNumber, sendingMsg, callback) => {
+
+
+	//validate params
+	phoneNumber = typeof(phoneNumber) == 'string' && phoneNumber.trim().length == 10? phoneNumber.trim() : false;
+	sendingMsg = typeof(sendingMsg) == 'string' && sendingMsg.trim().length > 0 && sendingMsg.trim().length < 1600 ? sendingMsg.trim() : false;
+
+	if(phoneNumber && sendingMsg){
+
+		//config request object for Twilio
+		let reqObj = {
+			'From': config.twilioVars.fromPhoneNumber,
+			'To': `+1${phoneNumber}`,
+			'Body': sendingMsg
+		}
+
+		//stringify the reqObj
+		let strReqOj = queryString.stringify(reqObj)
+
+		//config request details
+		let reqDetails = {
+			'protocol': 'https:',
+			'hostname': 'api.twilio.com',
+			'method': 'POST',
+			'path': `/2010-04-01/Accounts/${config.twilioVars.accountSid}/Messages.json`,
+			'auth': `${config.twilioVars.accountSid}:${config.twilioVars.authToken}`,
+			'headers':{
+				'Content-Type':'application/x-www-form-urlencoded',
+				'Content-Length': Buffer.byteLength(strReqOj)
+			}
+		}
+
+		//instantiate a req obj
+		let reqObjMethod = https.request(reqDetails, (res) => {
+
+			//grab status of the request
+			let resStatus = res.statusCode;
+
+			//callback the success
+			if(resStatus == 200 || resStatus == 201){
+				callback(false)
+			}else{
+				callback('Status code returned was '+resStatus);
+			}
+		});
+
+		//Bind to the error event SO the error does not get thrown
+		reqObjMethod.on('error', err => {
+			callback(e)
+		})
+
+		//add request obj to request
+		reqObjMethod.write(strReqOj)
+
+		//end request
+		reqObjMethod.end();
+
+	}else{
+		callback('given twilio arams were missing OR invalid');
+	}
+}
+
+
+module.exports = helpers;
+
+
+
+
+
+
