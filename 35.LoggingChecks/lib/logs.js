5fbd2b3dbddaa37ceafe717137b1c369af77630e
Eric Laursen <ericjlaursen@gmail.com>
2018-11-26 16:23:35 -0500

    log appending method developed.

--- 35.LoggingChecks/lib/logs.js
+++ 35.LoggingChecks/lib/logs.js
@@ -10,6 +10,43 @@ const zlib = require('zlib'); //compressing & decompressing
 //logs library initialization
 let logsLib = {};
 
+//base dir of the logs folder
+logsLib.baseDir = path.join(__dirname,'/../.logs/');
+
+//APPEND a string to a file
+//CREATE the file IF the file doesnt exist yet
+logsLib.append = (fileName, stringToAppend, callback) => {
+
+	//open the file for appending
+	//a switch is opening for appending
+	fs.open(`${logsLib.baseDir}${fileName}.log`,'a', (err, fileDescriptor) => {
+
+		if(!err && fileDescriptor){
+
+			//Append to file and close the file
+			fs.appendFile(fileDescriptor,`${stringToAppend}\n`, err => {
+				if(!err){
+
+					//close the file
+					fs.close(fileDescriptor, (err) => {
+						if(!err){
+							callback(false)
+						}else{
+							callback('error closing file being appended')
+						}
+					})
+
+				}else{
+					callback('error appending and closing the file')
+				}
+			})
+
+		}else{
+			callback('Couldnt open file for appending')
+		}
+	})
+}
+
 
 
 
