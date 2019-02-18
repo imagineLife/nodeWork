9810229b55fbf096618a56e33adeee5984a3677c
Eric Laursen <ericjlaursen@gmail.com>
2019-02-18 16:23:46 -0500

    returns error if non-matching, string if matching for now

--- pizzaAPI/lib/handlers/charge.js
+++ pizzaAPI/lib/handlers/charge.js
@@ -1,3 +1,6 @@
+//Dependencies
+const doTokens = require('./tokens')
+
 //holder of charge methods
 let charge = {}
 
@@ -14,6 +17,21 @@ charge.post = function(data,callback){
 	if(!passedToken){
 		callback(400, { Error: "Missing token" });
         return;
+	}else{
+
+		//GET all req'd fields from request payload
+		const dataEmail = data.payload.email
+
+		//verify that token is valid for passed phoneNumber
+		doTokens.verifyTokenMatch(passedToken, dataEmail, (tokenIsValid) => {
+			
+			//if non-matching token
+			if(!tokenIsValid){
+				callback(400, { Error: "non-matching token for this user" });
+			}else{
+				callback(200, {'Success': 'matching token here'})
+			}
+		})
 	}
 	
 }
