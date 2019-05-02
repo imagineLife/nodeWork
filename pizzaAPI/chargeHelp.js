258276d936c60f9bc789407bc2ddb5762018993a
Eric Laursen <ericjlaursen@gmail.com>
2019-05-02 06:36:37 -0400

    removes comments

--- pizzaAPI/chargeHelp.js
+++ pizzaAPI/chargeHelp.js
@@ -16,10 +16,6 @@ let charge = {}
 //	email in payload
 
 charge.post = function(data,callback){
-	console.log('charge post env vars => ')
-	console.log(process.env)
-	
-	//add stripeID in header req
 
 	//Prepare stripe communication details
 	let stripeAPIPrepData = {
@@ -29,30 +25,21 @@ charge.post = function(data,callback){
 
 	//log the DURATION of this charge post method
 	console.time('charge POST')
-	console.log('chargePOST data')
-	console.log(data)
-	console.log('// - * - * - * - * - //')
 
-	//store callback for easier access
-	// in related fns
 	charge.callback = callback;
 	
 	//GET token from headers
 	const passedToken = typeof(data.headers.token) == 'string' ? data.headers.token : false;
 	
-	//if no token return 400
 	if(!passedToken){
 		console.timeEnd('charge POST')
 		callback(400, { Error: "Missing token" });
         return;
 	}
 
-
-
 	//verify that token is valid for passed email
 	doTokens.verifyTokenMatch(passedToken, data.payload.email, (tokenIsValid) => {
 
-		//if non-matching token
 		if(!tokenIsValid){
 			console.timeEnd('charge POST')
 			callback(400, { Error: "non-matching token for this user" });
@@ -66,26 +53,16 @@ charge.post = function(data,callback){
 			cartTotal: null
 		}
 
-		//get user CART data from cart library
+		//get node-stored user CART data
 		dataLib.read('cart', data.payload.email, (err, cartData) => {
-			console.log('// - - - 2 - - //')
-			console.log('read user CART data');
-			console.log('cartData')
-			console.log(cartData)
-			
 
 			if(!cartData || cartData == undefined){
 				console.timeEnd('charge POST')
 				callback(400, {'Error': 'No Cart for a user with this token'})
 				return;
 			}
-
-			/*
-				get the cart  total
-				if there is user cart data
-				NOTE: must be in pennies for stripe ($10 gets translated to 1000 for stripe)
-			*/
 			
+			//calculate cart total
 			let thisCartTotal = cartData.cartData.reduce((acc, curVal) => {
 				return acc + (curVal.price * curVal.count) 
 			}, 0)
@@ -99,42 +76,32 @@ charge.post = function(data,callback){
 				- customer charging
 			*/
 
-		   	//prepare stripe customer user emailString string
 		    const emailStr = queryString.stringify({email: data.payload.email});
 
 			//check for stripe customer		
 			try{
 				
-				//check for stripeID passed through req payload
 				let existingStripeID = data.payload.stripeID || null
 
 				//if stripeID in payload
 				if(existingStripeID !== null){
-					console.log('IS stripeID in payload!!');
-					console.log('// - - - - - //')
-					
+				
 					thisCust = {id: existingStripeID}
 					charge.proceedWithStripeUser(thisCust, stripeCustomerDataObj, stripeAPIPrepData)
 				
-				// if no stripe customer
+				//Create New Stripe User
 				}else{
-
 					stripeAPIPrepData.method = "POST";
 
 				    try {
-				    	//Create New Stripe User
-				    	//returns customer object
-				        charge.makeStripeReq(stripeAPIPrepData, emailStr).then(res => {
+				    	
+				        charge.makeStripeReq(stripeAPIPrepData, emailStr).then(customerObj => {
 				        	
-				        	stripeCustomerDataObj.id = res.id;
+				        	stripeCustomerDataObj.id = customerObj.id;
 
-				        	// stre the stripe user ID in node user data
-				        	doUsers.patch({email: data.payload.email, stripeID: res.id}, (patchedData) => {
-				        		console.log('patchedData finished')
-				        		console.log(patchedData)
-				        	})
-
-				        	charge.proceedWithStripeUser(res, stripeCustomerDataObj, stripeAPIPrepData)
+				        	// store the stripe user ID in node user data
+				        	doUsers.patch({email: data.payload.email, stripeID: customerObj.id}, (patchedData) => return)
+				        	charge.proceedWithStripeUser(customerObj, stripeCustomerDataObj, stripeAPIPrepData)
 
 				        });
 				    } catch (error) {
@@ -142,7 +109,7 @@ charge.post = function(data,callback){
 				    	console.log(error)
 				    	console.timeEnd('charge POST')
 				       charge.callback(400, { Error: "Could not create a new customer" });
-				        return;
+				       return;
 				    }
 				} 
 
@@ -160,8 +127,6 @@ charge.post = function(data,callback){
 
 //prepare stripe communication object
 charge.prepRequestObj = (pathMethod) => {
-	console.log('pathMethod')
-	console.log(pathMethod)
 	
 	return {
         host: process.env.STRIPE_API_HOST,
@@ -171,26 +136,18 @@ charge.prepRequestObj = (pathMethod) => {
             Authorization: `Bearer ${process.env.STRIPE_API_TOKEN}`,
             Accept: "application/json",
             "Content-Type": "application/x-www-form-urlencoded",
-            /*
-					Not really necessary right now,
-					can help prevent against ddos attacks
-					maybe later...
-            */
+            //can help prevent against ddos attacks...
             // "Content-Length": Buffer.byteLength(reqStr)
         }
     };
 }
 
+//send to stripe
 charge.makeStripeReq = (reqObj, reqStr) => {
-	console.log('making stripe req, reqStr =>  ')
-	console.log(reqStr)
-	console.log('// -- -- -- //');
 
 	let stripeAPIResultData = null;
 	return new Promise(async function(resolve, reject) {
 
-		//can remove prepRequestObj
-		//can remove makeStripeSource
         try {
             stripeAPIResultData = await helpers.request(charge.prepRequestObj(reqObj), reqStr);
         } catch (error) {
@@ -203,9 +160,6 @@ charge.makeStripeReq = (reqObj, reqStr) => {
 }
 
 charge.proceedWithStripeUser = (res, stripeCustDataObj, stripeAPIPrepData) => {
-
-	console.log('// - - - 4 - - //')
-	console.log('proceedWithStripeUser, IS customer')
 	
 	/*
 
@@ -214,20 +168,15 @@ charge.proceedWithStripeUser = (res, stripeCustDataObj, stripeAPIPrepData) => {
 
 	*/	
 	
-	//set id
     stripeCustDataObj.id = res.id;
-    
-   	//Look for a source from stripe, if so, save source to var
 	stripeCustDataObj.source = (res.sources && res.sources.data && res.sources.data.length > 0 ) ? res.sources.data[0] : null
 
 	//IF NO SOURCE
 	//	 make a source
 	//	 charge the customer
 	if(stripeCustDataObj.source == null){
-		console.log('// - - - 5 - - //')
-		console.log('NO customer SOURCE yet, need to MAKE one');
 
-    	//Update stripe communication object
+
         stripeAPIPrepData = {
             path: `/v1/customers/${stripeCustDataObj.id}/sources`,
             method: "POST"
@@ -237,12 +186,8 @@ charge.proceedWithStripeUser = (res, stripeCustDataObj, stripeAPIPrepData) => {
 
 		//post a source for this customer
 		charge.makeStripeReq(stripeAPIPrepData, sourceString).then(stripeSource => {
-
-			console.log('// - - - 6 - - //')
-			console.log('POSTing to sources stripeSource')
 			
 			stripeCustDataObj.source = stripeSource
-
 			charge.chargeStripeCustomer(stripeAPIPrepData, stripeCustDataObj)
 
 		})
@@ -252,7 +197,6 @@ charge.proceedWithStripeUser = (res, stripeCustDataObj, stripeAPIPrepData) => {
 	//if stripe source IS present
 	//	 charge the customer
 	if(stripeCustDataObj.source !== null) {
-		
 		charge.chargeStripeCustomer(stripeAPIPrepData, stripeCustDataObj);
 	}
 }
@@ -273,9 +217,6 @@ charge.chargeStripeCustomer = (stripeAPIPrepData, stripeCustDataObj) => {
 			path: "/v1/charges",
 			method: "POST"
 		};
-
-		console.log('// - - - 7 - - //')
-		console.log('ALREADY SOURCES stripeCustDataObj.cartTotal')
 
 		let dataInString = charge.prepChargeReqStr(stripeCustDataObj);
 
