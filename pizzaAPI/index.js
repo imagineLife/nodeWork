ea4e671c82191d626cf73e02bb0d0926fcf3995d
Eric Laursen <ericjlaursen@gmail.com>
2019-05-15 06:25:29 -0400

    clean-up

--- pizzaAPI/index.js
+++ pizzaAPI/index.js
@@ -1,6 +1,6 @@
 //PRIMARY file for the api
 const server = require('./lib/server')
-const webWorkers = require('./lib/workers')
+
 const envVars = require('./env.js')
 
 //START APP with cli_env=prod or cli_env=dev node index.js
@@ -9,21 +9,15 @@ const envVars = require('./env.js')
 let serverObj = {};
 
 //init function
-//starts server & webWorkers
+//starts server
 serverObj.init = () => {
 
 	//start server
 	server.init();
 
-	//start webWorkers
-	// webWorkers.init()
-
-
 	//get/set env vars from env file
 	const envType = process.env.cli_env || 'dev'
 	for(var thisEnv in envVars[envType]){
-		// console.log('looping env vars')
-		// console.log(thisEnv)
 		process.env[thisEnv] = envVars[envType][thisEnv]
 	}
 	
