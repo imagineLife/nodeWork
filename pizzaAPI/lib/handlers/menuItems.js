a601e0552aa70568cb2aca5954fd6e07cbc29529
Eric Laursen <ericjlaursen@gmail.com>
2019-05-15 16:32:40 -0400

    updates sanity checking && indentation

--- pizzaAPI/lib/handlers/menuItems.js
+++ pizzaAPI/lib/handlers/menuItems.js
@@ -3,46 +3,42 @@ const dataLib = require('../data')
 const doTokens = require('./tokens')
 
 
-/*
-	Get Menu Items Handling
-*/
-
 let doMenuItems = {};
 
-doMenuItems.get = (data, callback) => {
-	//TEST this by using postman with
-	// http://localhost:3000/menuItems?email=jajo@gmail.com
+//TEST this by using postman with
+	// http://localhost:3000/menuItems?email={emailHere}
 	// should return the menuItem object
+doMenuItems.get = (data, callback) => {
+
 	let dataEmail = data.queryStrObj.email;
 	//check that email is valid
 	const email = typeof(dataEmail) == 'string' && dataEmail.includes('@') && dataEmail.includes('.com') ? dataEmail.trim() : false;
 	//GET token from headers
 	const passedToken = typeof(data.headers.token) == 'string' ? data.headers.token : false;
 
-	if(email && passedToken){
-
-		//verify the token matches
-		doTokens.verifyTokenMatch(passedToken, email, (tokenIsValid) => {
-
-			if(tokenIsValid){
-				let items;
-				try{
-					//lookup the menuItems from the filesystem
-					items = dataLib.readSync('menuItems','menuItems');
-					callback(200, {'MenuItems': JSON.parse(items)})
-				}
-				catch(err){
-					callback(403, {'Error': err.code})
-					return;
-				}
-			}else{
-				callback(403, {'Error': 'Non-Matching user token'})
-			}
-		})
-
-	}else{
+	if(!email || !passedToken){
 		callback(400, {'Error': 'Specified token NOT there'})
+		return
 	}
+
+	//verify the token matches
+	doTokens.verifyTokenMatch(passedToken, email, (tokenIsValid) => {
+
+		if(!tokenIsValid){
+			callback(403, {'Error': 'Non-Matching user token'})
+			return;
+		}
+		let items;
+		try{
+			//lookup the menuItems from the filesystem
+			items = dataLib.readSync('menuItems','menuItems');
+			callback(200, {'MenuItems': JSON.parse(items)})
+		}
+		catch(err){
+			callback(403, {'Error': err.code})
+			return;
+		}
+	})
 }
 
 module.exports = doMenuItems;
