d41639c1c3cbfbb2cffed1a68d153d7b662f69fc
Eric Laursen <ericjlaursen@gmail.com>
2019-08-16 13:48:10 -0400

    removes nesting of if/elses

--- 26.verifyMatchingToken/lib/handlers.js
+++ 26.verifyMatchingToken/lib/handlers.js
@@ -51,68 +51,56 @@ routeHandlers.doUsers.post = function(data,callback){
 	const pw = checkForLengthAndType(data.payload.passWord)
 	const tosAg = typeof(dataTos) == 'boolean' && dataTos == true ? true : false;
 
-
-	//continue is fall reqd fields are present
-	if(fn && ln && pn && pw && tosAg){
-
-		/*
-			make sure that user doesn't already exist
-			USING the CRUD handlers from the data directory as a dependence above
-			READ from users data using this data
-		*/
-
-		//check if user phoneNumber already exists
-		//takes dir, fileName,callback
-		dataLib.read('users', pn, (err,result) => {
-
-			//if it comes back with an error,
-			// that means there IS no phone number
-			if(err){
-				//Hash the password using built in library called crypto,
-				//created in HELPERS file,
-				//included in dependencies
-				const hashedPW = helpers.hash(pw);
-
-				//if the hashing succeeded save the user data
-				//else below
-				if(hashedPW){
-					//create a user object from user data
-					let userObj = {
-						firstName: fn,
-						lastName: ln,
-						phone: pn,
-						hashedPW: hashedPW,
-						tosAgreement: true
-					}
-
-					//STORE this user to disk
-					//create method takes dir,fileName,data,callback
-					dataLib.create('users',pn,userObj,(err) => {
-						if(!err){
-							callback(200, {'Success!': `User ${userObj.firstName} created successfully!`})
-						}else{
-							console.log('create user error')
-							console.log(err)
-							callback(500, {'ERROR': 'Could not create the new user'})
-						}
-					})
-				}else{
-					callback(500, {'ERROR': 'Could not hash'})
-				}
-
-
-			}else{
-				//User already exists
-				callback(400,{'Error': 'A User with that number already exists'})
-			}
-		})
-
-	
-	}else{
 	//THROW ERROR if payload doesn't contain req'd fields
-		callback(400,{'Error': 'Missing Reqd fields'})
+	if(!fn || !ln || !pn || !pw || !tosAg){
+		return callback(400,{'Error': 'Missing Reqd fields'})
 	}
 
+	/*
+		make sure that user doesn't already exist
+		USING the CRUD handlers from the data directory as a dependence above
+		READ from users data using this data
+	*/
+
+	//check if user phoneNumber already exists
+	//takes dir, fileName,callback
+	dataLib.read('users', pn, (err,result) => {
+
+		//User already exists
+		if(!err){
+			return callback(400,{'Error': 'A User with that number already exists'})
+		}
+		//Hash the password using built in library called crypto,
+		//created in HELPERS file,
+		//included in dependencies
+		const hashedPW = helpers.hash(pw);
+
+		//if the hashing succeeded save the user data
+		//else below
+		if(!hashedPW){
+			return callback(500, {'ERROR': 'Could not hash'})
+		}
+		//create a user object from user data
+		let userObj = {
+			firstName: fn,
+			lastName: ln,
+			phone: pn,
+			hashedPW: hashedPW,
+			tosAgreement: true
+		}
+
+		//STORE this user to disk
+		//create method takes dir,fileName,data,callback
+		dataLib.create('users',pn,userObj,(err) => {
+			if(!err){
+				callback(200, {'Success!': `User ${userObj.firstName} created successfully!`})
+			}else{
+				console.log('create user error')
+				console.log(err)
+				callback(500, {'ERROR': 'Could not create the new user'})
+			}
+		})
+	})
 }
 
 //Users PUT
@@ -131,58 +119,62 @@ routeHandlers.doUsers.put = function(data,callback){
 	const pw = checkForLengthAndType(data.payload.passWord)
 
 	//if phone number exists, keep going
-	if(phoneNumber){
+	if(!phoneNumber){
+		return callback(400, {'Error': 'Missing reqd field'})
+	}
 
-		//if at least one other field exists to update
-		if(fn || ln || pw){
+	//get token
+	const passedToken = data.headers.token || null;
+	
+	if(!passedToken){
+		return callback(403, {"Error": "missing token in header"})
+	}
 
-			//lookup the user
-			dataLib.read('users', phoneNumber, (err, userData) => {
-				
-				//check if file is error-less AND has userdata
-				if(!err && userData){
-
-					//update the field in the userData 
-					if(fn){
-						userData.firstName = fn;
-					}
-					if(ln){
-						userData.lastName = ln;
-					}
-					if(pw){
-						userData.passWord = helpers.hash(pw);
-					}
-
-					//Store the newly updated userData obj
-					dataLib.update('users', phoneNumber, userData, (err) => {
-
-						if(!err){
-							callback(200)
-						}else{
-							console.log('err!')
-							console.log(err)
-							ccallback(500, {'Error': 'Couldnt update this user with this info'})
-						}
-
-					})
-
-
-				//if error or no data for that file
-				}else{
-					callback(400, {'Error': 'No data or file exists for that'})
-				}
-			})
-
-		//if no other field is present to update
-		}else{
-			callback(400, {'Error': 'Missing updatable field'})
+	routeHandlers.doTokens.verifyTokenMatch(passedToken, phoneNumber, (err, tokenIsValid) => {
+		
+		if(!tokenIsValid){
+			return callback(403, {"ERROR": "invalid token"})
 		}
 
+		//if no other field is present to update
+		if(!fn || !ln || !pw){
+			return callback(400, {'Error': 'Missing updatable field'})
+		}
 
-	//if phone is invalid, Error 
-	}else{
-		callback(400, {'Error': 'Missing reqd field'})
-	}
+		//lookup the user
+		dataLib.read('users', phoneNumber, (err, userData) => {
+			
+			//check if file is error-less AND has userdata
+			//if error or no data for that file
+			if(err || !userData){
+				return callback(400, {'Error': 'No data or file exists for that'})
+			}
+
+			//update the field in the userData 
+			if(fn){
+				userData.firstName = fn;
+			}
+			if(ln){
+				userData.lastName = ln;
+			}
+			if(pw){
+				userData.passWord = helpers.hash(pw);
+			}
+
+			//Store the newly updated userData obj
+			dataLib.update('users', phoneNumber, userData, (err) => {
+
+				if(!err){
+					return callback(200)
+				}else{
+					console.log('err!')
+					console.log(err)
+					return callback(500, {'Error': 'Couldnt update this user with this info'})
+				}
+
+			})
+		})
+	})
 }
 
 //Users GET
@@ -198,40 +190,38 @@ routeHandlers.doUsers.get = function(data,callback){
 	const phoneNumber = typeof(data.queryStrObj.phoneNumber) == 'string' && data.queryStrObj.phoneNumber.trim().length == 10 ? data.queryStrObj.phoneNumber.trim() : false;
 
 	//if phone is valid
-	if(phoneNumber){
+	if(!phoneNumber){
+		return callback(400, {'Error': 'Seems like Missing phoneNumber field'})
+	}
 
-		//get token
-		const passedToken = data.headers.token || null;
+	//get token
+	const passedToken = data.headers.token || null;
+	
+	if(!passedToken){
+		callback(403, {"Error": "missing token in header"})
+	}
+
+	routeHandlers.doTokens.verifyTokenMatch(passedToken, phoneNumber, (err, tokenIsValid) => {
 		
-		if(!passedToken){
-			callback(403, {"Error": "missing token in header"})
+		if(!tokenIsValid){
+			return callback(403, {"ERROR": "invalid token"})
 		}
 
-		routeHandlers.doTokens.verifyTokenMatch(passedToken, phoneNumber, (err, tokenIsValid) => {
-			
-			if(!tokenIsValid){
-				return callback(403, {"ERROR": "invalid token"})
+		//lookup the user from the filesystem
+		dataLib.read('users',phoneNumber, (err, storedUserData) => {
+			if(!err && storedUserData){
+
+				//REMOVE hashed pw from the user object before showing the user
+				delete storedUserData.hashedPW;
+				callback(200, storedUserData);
+
+			}else{
+
+				//NOT FOUND USER
+				callback(404)
 			}
-
-			//lookup the user from the filesystem
-			dataLib.read('users',phoneNumber, (err, storedUserData) => {
-				if(!err && storedUserData){
-
-					//REMOVE hashed pw from the user object before showing the user
-					delete storedUserData.hashedPW;
-					callback(200, storedUserData);
-
-				}else{
-
-					//NOT FOUND USER
-					callback(404)
-				}
-			})
 		})
-
-	}else{	
-		callback(400, {'Error': 'Seems like Missing phoneNumber field'})
-	}
+	})
 	
 }
 
