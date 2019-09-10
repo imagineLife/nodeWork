a338982c6844fed4419eabbb4837a4119a09a60d
Eric Laursen <ericjlaursen@gmail.com>
2019-09-10 05:30:28 -0400

    mimmic prev exampl, code-cleanup

--- pizzaAPI/lib/handlers/tokens.js
+++ pizzaAPI/lib/handlers/tokens.js
@@ -15,38 +15,33 @@ let doTokens = {};
 
 
 //Tokens post
-//REQd data - email, pw
+//REQd data - phone, pw
 //a user creating a  token to use later
 //NO optional data
 doTokens.post = (data, callback) => {
-	debug('\x1b[32m\x1b[37m%s\x1b[0m','POST Data:')
-	debug(data);
-	
-	//parse email & pw
-	let dataEmail = data.payload.email
-	const eml = typeof(dataEmail) == 'string' && dataEmail.includes('@') && dataEmail.includes('.com') ? dataEmail.trim() : false;
+	let dataPhone = data.payload.phoneNumber
+	//parse phone & pw
+	const pn = typeof(dataPhone) == 'string' && dataPhone.trim().length == 10 ? dataPhone.trim() : false;
 	const pw = checkForLengthAndType(data.payload.passWord);
-	
-	if(!eml && !pw){
-		callback(400,{'Error': 'Missing phone or pw'})
-		return;
+
+	if(!pn || !pw){
+		return callback(400,{'Error': 'Missing phone or pw'})
 	}
 
-	//lookup user who matches the email
-	dataLib.read('users', eml, (err, userData) => {
+	//lookup user who matches the phoneNumber
+	dataLib.read('users', pn, (err, userData) => {
 
-		//sanity check
-		if(err || !userData){
-			callback(400, {'Error': 'Couldnt find that user'})
+		//sanity check response
+		if(err || userData == 'undefined'){
+			return callback(400, {'Error': 'Couldnt find that user by phoneNumber'})
 		}
 
 		//hash pw to compare to STORED hashed pw
 		const hashedPW = helpers.hash(pw);
 
-		//check for non-matching passwords, saved && sent through request
+		//check if hashed pw is same as SAVED hashed pw
 		if(hashedPW !== userData.hashedPW){
-			callback(400, {'Error': 'PW did not match the stored pw'})
-			return;
+			return callback(400, {'Error': 'PW did not match the stored pw'})
 		}
 
 		//create new TOKEN for this user
@@ -54,35 +49,31 @@ doTokens.post = (data, callback) => {
 
 		//set exp date 1 hour in the future
 		const expDate = Date.now() + 1000 * 60 * 60;
-		
+
 		//store the tokenId as a 'token Object'
 		const tokenObj = {
-			email: eml,
+			phone: pn,
 			tokenId: tokenId,
-			expires: expDate,
-			stripeID: userData.stripeID || null
+			expires: expDate
 		}
 
 		//store the tokenObj
 		//NAME the file the tokenID
 		dataLib.create('tokens', tokenId, tokenObj, (err) => {
-			if(!err){
-				callback(200, tokenObj)
-			}else{
-				callback(500, {'Error' : 'Couldnt create new token'})
+			
+			if(err){
+				return callback(500, {'Error' : 'Couldnt create new token'});
 			}
+
+			return callback(200, tokenObj)
 		})
-
 	})
-
 }
 
 //Tokens get
 //reqd data is ID
 //NO opt data
 doTokens.get = (data, callback) => {
-	debug('\x1b[32m\x1b[37m%s\x1b[0m','GET Data:')
-	debug(data);
 
 	//TEST this by using postman with
 	// http://localhost:3000/users?phoneNumber=1238675309
@@ -92,27 +83,21 @@ doTokens.get = (data, callback) => {
 	const id = typeof(data.queryStrObj.id) == 'string' && data.queryStrObj.id.trim().length == 19 ? data.queryStrObj.id.trim() : false;
 
 	//if id is valid
-	if(id){
-
-		//lookup the user from the filesystem
-		dataLib.read('tokens',id, (err, storedTokenData) => {
-			
-			if(!err && storedTokenData){
-
-				//REMOVE hashed pw from the user object before showing the user
-				callback(200, storedTokenData);
-
-			}else{
-
-				//NOT FOUND USER
-				callback(404)
-			}
-		})
-
-	}else{	
-		callback(400, {'Error': 'Seems like incorrect token id'})
+	if(!id || id == 'undefined'){
+		return callback(400, {'Error': 'Seems like incorrect token id'})
 	}
 
+	//lookup the user from the filesystem
+	dataLib.read('tokens',id, (err, storedTokenData) => {
+		
+		if(err || !storedTokenData){
+			//NOT FOUND USER
+			return callback(404)
+		}
+
+		//REMOVE hashed pw from the user object before showing the user
+		return callback(200, storedTokenData);
+	})
 }
 
 /*
@@ -121,110 +106,98 @@ doTokens.get = (data, callback) => {
 	NO optional data
 */
 doTokens.put = (data, callback) => {
-	debug('\x1b[32m\x1b[37m%s\x1b[0m','PUT Data:')
-	debug(data);
+	
 	//get id & extend boolean from payload
-	const id = typeof(data.payload.id) == 'string' && data.payload.id.trim().length == 19 ? data.payload.id.trim() : false;
-	const extend = typeof(data.payload.extend) == 'boolean' && data.payload.extend == true ? true : false;
+	const tokenID = typeof(data.payload.token) == 'string' && data.payload.token.trim().length == 19 ? data.payload.token.trim() : false;
+	// const extend = typeof(data.payload.extend) == 'boolean' && data.payload.extend == true ? true : false;
 
-	if(id && (extend == true)){
-
-		//lookup token based on id
-		dataLib.read('tokens',id, (err,tokenData) => {
-
-			if(!err && tokenData){
-
-				//check for EXPIRED token
-				if(tokenData.expires > Date.now()){
-
-					//set expiration of the token an hour from now
-					tokenData.expires = Date.now()  + 1000 * 60 * 60;
-
-					//store the new token data
-					dataLib.update('tokens', id, tokenData, (err) => {
-
-						if(!err){
-							callback(200)
-						}else{
-							callback(500, {'Error': 'Couldnt update the token exp for some reason'})
-						}
-
-					})
-
-				}else{
-					callback(400, {'Error': 'The token has already expired & cannot be extended'})
-				}
-
-
-			}else{
-				callback(400, {'Error': 'Specified token NOT there'})
-			}
-
-		})
-
-	}else{
-		callback(400, {'Error':'missing id or extendTrueVal'})
+	// if(!tokenID || !(extend == true)){
+	if(!tokenID){		
+		return callback(400, {'Error':'missing token'})
 	}
 
+	//lookup token based on id
+	dataLib.read('tokens',tokenID, (err,tokenData) => {
+
+		if(err || !tokenData){
+			return callback(400, {'Error': 'Specified token NOT there'})
+		}
+		
+		let notExpired = tokenData.expires > Date.now()
+		//check for EXPIRED token
+		if(!notExpired){
+			return callback(400, {'Error': 'The token has already expired & cannot be extended'})
+		}
+
+		//set expiration of the token an hour from now
+		tokenData.expires = Date.now()  + 1000 * 60 * 60;
+
+		//store the new token data
+		dataLib.update('tokens', tokenID, tokenData, (err) => {
+
+			if(err){
+			  return callback(500, {'Error': 'Couldnt update the token exp for some reason'})
+			}
+
+			return callback(200, {"Success": "Check Updated"})
+		})
+	})
 }
 
 //Tokens delete
 //required: id
 //optional: NONE
 doTokens.delete = (data, callback) => {
-	debug('\x1b[32m\x1b[37m%s\x1b[0m','DELETE Data:')
-	debug(data);
+	
 	//check that id is valid
 	const id = typeof(data.queryStrObj.id) == 'string' && data.queryStrObj.id.trim().length == 19 ? data.queryStrObj.id.trim() : false;
 
 	//if id is valid
-	if(id){
-
-		//lookup the token from the filesystem
-		dataLib.read('tokens',id, (err, storedUserData) => {
-			
-			if(!err && storedUserData){
-
-				//REMOVE user
-				dataLib.delete('tokens', id, (err) => {
-
-					if(!err){
-						callback(200, {'DELETED': 'Successfully'})
-					}else{
-						callback(500, {'Error' :'Couldnt delete this user for some odd reason'})
-					}
-
-				})
-			}else{
-				//NOT FOUND USER
-				callback(400, {'Error': 'Couldnt Find token by id'})
-			}
-		})
-
-	}else{	
-		callback(400, {'Error': 'Seems like Missing id field'})
+	if(!id){
+		return callback(400, {'Error': 'Seems like Missing id field'})
 	}
 
+	//lookup the token from the filesystem
+	dataLib.read('tokens',id, (err, storedUserData) => {
+		
+		//sanity check the token/user data
+		if(err || !storedUserData){
+			return callback(400, {'Error': 'Couldnt Find token by id'})
+		}
+
+		//REMOVE user
+		dataLib.delete('tokens', id, (err) => {
+
+			if(err){
+				return callback(500, {'Error' :'Couldnt delete this user for some odd reason'})
+			}
+
+			return callback(200, {'DELETED': 'Successfully'})
+		})
+	})
 }
 
 
 //VERIFY that a given tokenID MATCHES a given user
-doTokens.verifyTokenMatch = function(tokenID,givenEmailAddr,callback){
-	debug('\x1b[32m\x1b[37m%s\x1b[0m','VERIFY-MATCH Data:')
-	debug(tokenID);
-	debug(givenEmailAddr)
+doTokens.verifyTokenMatch = function(tokenID,phoneNumber,callback){
+	
 	//read the token by id
 	dataLib.read('tokens',tokenID, (err, storedTokenData) => {
-		if(!err && storedTokenData){
-			//Check that the tokenID MATCHES the given user AND has not expired
-			if(storedTokenData.email == givenEmailAddr && storedTokenData.expires > Date.now()){
-				callback(true)
-			}else{
-				callback(false)
-			}
-		}else{
-			callback(false)
+
+		//sanity-checking
+		if(err || !storedTokenData){
+			return callback(false)
 		}
+
+		const tokenStillValid = storedTokenData.expires > Date.now();
+
+		//Check that the tokenID MATCHES the given user AND has not expired
+		if((storedTokenData.phone !== phoneNumber) || (tokenStillValid !== true)){
+			return callback(false)
+		}
+
+		return callback(true)
+
 	})
 }
 
