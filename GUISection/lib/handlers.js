530f26caf2f2929b73d66957295f220f1eb0cb52
Eric Laursen <ericjlaursen@gmail.com>
2019-09-11 12:32:49 -0400

    de-nests token.put

--- GUISection/lib/handlers.js
+++ GUISection/lib/handlers.js
@@ -580,45 +580,34 @@ routeHandlers.doTokens.put = (data, callback) => {
 	const id = typeof(data.payload.id) == 'string' && data.payload.id.trim().length == 19 ? data.payload.id.trim() : false;
 	const extend = typeof(data.payload.extend) == 'boolean' && data.payload.extend == true ? true : false;
 
-	if(id && (extend == true)){
-
-		//lookup token based on id
-		dataLib.read('tokens',id, (err,tokenData) => {
-
-			if(!err && tokenData){
-
-				//check for EXPIRED token
-				if(tokenData.expires > Date.now()){
-
-					//set expiration of the token an hour from now
-					tokenData.expires = Date.now()  + 1000 * 60 * 60;
-
-					//store the new token data
-					dataLib.update('tokens', id, tokenData, (err) => {
-
-						if(!err){
-							callback(200)
-						}else{
-							callback(500, {'Error': 'Couldnt update the token exp for some reason'})
-						}
-
-					})
-
-				}else{
-					callback(400, {'Error': 'The token has already expired & cannot be extended'})
-				}
-
-
-			}else{
-				callback(400, {'Error': 'Specified token NOT there'})
-			}
-
-		})
-
-	}else{
-		callback(400, {'Error':'missing id or extendTrueVal'})
+	if(!(id && (extend == true))){
+		return callback(400, {'Error':'missing id or extendTrueVal'})
 	}
 
+	//lookup token based on id
+	dataLib.read('tokens',id, (err,tokenData) => {
+
+		if(err || !tokenData){
+			return callback(400, {'Error': 'Specified token NOT there'})
+		}
+
+		//check for EXPIRED token
+		if(!(tokenData.expires > Date.now())){
+			return callback(400, {'Error': 'The token has already expired & cannot be extended'})
+		}
+
+		//set expiration of the token an hour from now
+		tokenData.expires = Date.now()  + 1000 * 60 * 60;
+
+		//store the new token data
+		dataLib.update('tokens', id, tokenData, (err) => {
+
+			if(err){
+				return callback(500, {'Error': 'Couldnt update the token exp for some reason'})
+			}
+				callback(200)
+		})
+	})
 }
 
 //Tokens delete
