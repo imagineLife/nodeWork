eed05a179ce41d3a024078e9bb567a911b54f853
Eric Laursen <ericjlaursen@gmail.com>
2020-01-24 18:38:04 -0500

    first test ++ export

--- Stabilizing/test/unit.js
+++ Stabilizing/test/unit.js
@@ -11,3 +11,13 @@ var assert = require('assert');
 
 // Holder for Tests
 var unit = {};
+
+// Assert that the getANumber function is returning a number
+unit['helpers.getANumber should return a number'] = function(done){
+  var val = helpers.getANumber();
+  assert.equal(typeof(val), 'number');
+  done();
+};
+
+// Export the tests to the runner
+module.exports = unit;
