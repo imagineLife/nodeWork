38432238dc30241e5c0c1c47a5ae7a536581af27
Eric Laursen <ericjlaursen@gmail.com>
2020-01-27 05:38:51 -0500

    more

--- FEM/console/script.js
+++ FEM/console/script.js
@@ -7,4 +7,24 @@
 	this will execute similar to a bash script
 	pronounced shibang
 	/usr/bin/env is a helper that looks for the executable node
+
+	make this an executable
+	CHECK executable status using terminal
+	ls -la
+	the first column, -rwxr--r--
+	if the 'x' is not there, cant execute it
+	to make it executable
+	chmod u+x script.js
+	NOW i can run the file using ./script.js
+	crazy
 */ 
+
+printHelp()
+// **********
+function printHelp(){
+	console.log('script usage');
+	console.log('script.js --help');
+	console.log('');
+	console.log('--help      print this help');
+	console.log('');
+}
