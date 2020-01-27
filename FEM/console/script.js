56b5d0c5fb46223fa5f8fd5606ea158978451623
Eric Laursen <ericjlaursen@gmail.com>
2020-01-26 20:43:09 -0500

    notes about running in shell

---
+++ FEM/console/script.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+"use strict";
+
+/*
+	that top line tells the shell to INTERPRET this code
+	this will execute similar to a bash script
+	pronounced shibang
+	/usr/bin/env is a helper that looks for the executable node
+*/ 
