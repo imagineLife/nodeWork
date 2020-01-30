802a17de0c93cbde38373b7d144ae2fce31f5ac7
Eric Laursen <ericjlaursen@gmail.com>
2020-01-30 18:52:17 -0500

    http req deets

--- Stabilizing/test/api.js
+++ Stabilizing/test/api.js
@@ -6,3 +6,23 @@ const app = require('./../index')
 const assert = require('assert')
 const http = require('http')
 const config = require('./../lib/config')
+
+let api = {}
+
+let helpers = {}
+helpers.getReq = function(passedPath,cb){
+
+	//config the req details
+	const reqDeeets = {
+		'protocol':'',
+		'hostname': 'localhost',
+		'port': config.httpPort,
+		'method': 'GET',
+		'path': passedPath,
+		'headers': {
+			'Content-Type': 'application/json'
+		} 
+	}
+}
+
+module.exports = api
