7a89af7b258dfba87cf6ddb983b96a881cb9ddd5
Eric Laursen <ericjlaursen@gmail.com>
2020-02-08 20:25:29 -0500

    get or false hostname from parsedURL

--- PerfOpt/lib/handlers.js
+++ PerfOpt/lib/handlers.js
@@ -881,6 +881,7 @@ routeHandlers.doChecks.post = (data, callback) => {
 
 			//PERFORMANCE OPTIMIZATIONS
 			const parsedURL = nodeURL.parse(`${sentProtocol}://${sentUrl}`, true)
+			const hostName = typeof(parsedURL.hostname) == 'string' && parsedURL.hostname.lenght > 0 ? parsedURL.hostname : false;
 
 			//CREATE new check
 			//rndm ID
