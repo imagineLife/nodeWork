8b0da9397f361d5c65674f89041017aab323cd65
Eric Laursen <ericjlaursen@gmail.com>
2020-02-11 05:49:05 -0500

    a vm

---
+++ misc/vm/index.js
@@ -0,0 +1,23 @@
+/*
+	NODE VMs
+	https://nodejs.org/api/vm.html#vm_vm_executing_javascript
+	...enables running js (+ more?!) in a v8 VM 'context'
+*/ 
+
+var vm = require('vm');
+
+// Define a context for the script to run in
+var context = {
+  'foo' : 25
+};
+
+// Define the script
+var script = new vm.Script(`
+  foo = foo * 2;
+  var bar = foo + 1;
+  var fizz = 52;
+`);
+
+// Run the script
+script.runInNewContext(context);
+console.log(context);
