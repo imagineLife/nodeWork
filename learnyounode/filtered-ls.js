256744d1bdc7ebb9bb83261c90cfc54ebfb72e28
Eric Laursen <ericjlaursen@gmail.com>
2020-04-21 16:18:49 -0400

    more

--- learnyounode/filtered-ls.js
+++ learnyounode/filtered-ls.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 const path = require('path');
-const friendlyExt = '.'+ process.argv[3]
+const friendlyExt = `.${process.argv[3]}`;
 const dirToRead = process.argv[2]
 fs.readdir(dirToRead, (err, dirList) => {
 	if(err){
--- learnyounode/order.md
+++ learnyounode/order.md
@@ -1,3 +1,4 @@
 1. Baby-steps
 2. my-first-io
 3. my-first-async-io
+4. filtered-ls
