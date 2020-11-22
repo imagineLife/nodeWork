04b1a2088267c71abfebf6e2da42490f311db1fc
Eric Laursen <ericjlaursen@gmail.com>
2020-11-22 14:16:50 -0500

    init

---
+++ in-depth/js-deets/scope.js
@@ -0,0 +1,25 @@
+/*
+  SCOPE
+  - where to look for stuff
+  
+  A VARIABLE is either...
+    -  getting assigned a value
+      let x = "string"
+    - recalling a previously stored value
+      console.log(x)
+  
+  A COMPILER 'figures out' the scope
+  - compiling is a step within js
+  - js doesnt REALLY run top-down
+    - JS is not an interpreted language, it is compiled
+    - ... or at leasted PARSED if not compiled
+    - i.e when a syntax error is present, the WHOLE PROGRAM does not run
+    - THIS is unlike bash...
+      - bash will run 1-9 when there's an error on line 10
+  - COMPILER THEORY, 4 stages
+    - lexing
+    - tokenization
+    - parsing - turns stream of tokens into abstract syntax tree
+    - code generation - take abs. synt. tree && producting executable form of a program
+  - 
+*/ 
