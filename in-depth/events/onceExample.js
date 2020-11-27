62b01241dfbf9f27c232f62f1210dc610cd9ff11
Eric Laursen <ericjlaursen@gmail.com>
2020-11-26 19:38:18 -0500

    example of once

--- in-depth/events/onceExample.js
+++ in-depth/events/onceExample.js
@@ -1,21 +1,21 @@
-'use strict';
-const assert = require('assert');
-const { EventEmitter } = require('events');
-const thisTry = new EventEmitter();
-let count = 0;
+// 'use strict';
+// const assert = require('assert');
+// const { EventEmitter } = require('events');
+// const thisTry = new EventEmitter();
+// let count = 0;
 
-thisTry.once('tick', listener);
+// thisTry.once('tick', listener);
 
-// listener gets called 1x
-setInterval(() => {  
-  thisTry.emit('tick')
-}, 100);
+// // listener gets called 1x
+// setInterval(() => {  
+//   thisTry.emit('tick')
+// }, 100);
 
-function listener () {  
-  count++  
-  setTimeout(() => {
-    assert.equal(count, 1)
-    assert.equal(this, thisTry)
-    console.log('passed!')  
-  }, 250)
-}
+// function listener () {  
+//   count++  
+//   setTimeout(() => {
+//     assert.equal(count, 1)
+//     assert.equal(this, thisTry)
+//     console.log('passed!')  
+//   }, 250)
+// }
