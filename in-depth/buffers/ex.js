7161b14826936444e3f94b4263c12e8ef4d21cd3
Eric Laursen <ericjlaursen@gmail.com>
2020-11-28 08:15:22 -0500

    comments

--- in-depth/buffers/ex.js
+++ in-depth/buffers/ex.js
@@ -8,9 +8,17 @@ bB = bA.slice(2,3)
 // { bB: <Buffer 00> }
 console.log({bB})
 
-// re-write an element
+/*
+  re-write an element
+*/
 bB[0] = 100;
+
 // { bB: <Buffer 64> }
 console.log({bB})
+
 //  {it: 100 }
 console.log({it: bB[0]})
+
+// Change in of initial buffer
+// { bA: <Buffer 00 00 64 00 00 00 00 00 00 00> }
+console.log({bA})
