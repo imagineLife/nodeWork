9bd6cc3e39526c0b80b499d6a7585357e2836bc5
Eric Laursen <ericjlaursen@gmail.com>
2020-11-28 08:18:32 -0500

    uint8Array init

--- in-depth/buffers/ex.js
+++ in-depth/buffers/ex.js
@@ -22,3 +22,16 @@ console.log({it: bB[0]})
 // Change in of initial buffer
 // { bA: <Buffer 00 00 64 00 00 00 00 00 00 00> }
 console.log({bA})
+
+
+/*
+  New Example, Uint8Array @ length of 10
+*/
+bC = new Uint8Array(10)
+/*
+  { bC: Uint8Array(10) [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] }
+*/
+console.log({bC})
+bD = bC.slice(2,3);
+// { bD: Uint8Array(1) [ 0 ] }
+console.log({bD})
