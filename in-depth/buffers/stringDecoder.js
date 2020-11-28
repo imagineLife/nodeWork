0a62d36e7ec1858aacbeb09447b36fc27e80054e
Eric Laursen <ericjlaursen@gmail.com>
2020-11-28 10:00:19 -0500

    node api example

--- in-depth/buffers/stringDecoder.js
+++ in-depth/buffers/stringDecoder.js
@@ -6,3 +6,12 @@ console.log(`2: `, frag2.toString()) // prints ��
 const decoder = new StringDecoder()
 console.log(`3: `, decoder.write(frag1)) // prints nothing
 console.log(`4: `, decoder.write(frag2)) // prints
+
+
+const decodeUTF = new StringDecoder('utf8');
+
+const cent = Buffer.from(0xC2, 0xA2]);
+console.log(`5: `,decodeUTF.write(cent));
+
+const euro = Buffer.from([0xE2, 0x82, 0xAC]);
+console.log(`6: `,decodeUTF.write(euro));
