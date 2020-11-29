2d0f1a69f5079dd010a8a4311c62cecbee3eed71
Eric Laursen <ericjlaursen@gmail.com>
2020-11-29 18:59:58 -0500

    logs

--- in-depth/buffers/jsonBuffer.js
+++ in-depth/buffers/jsonBuffer.js
@@ -1,3 +1,25 @@
 const str = `dummy string here a`;
-const jsonBuff = Buffer.from(str).toJSON()
+const bufferFromString = Buffer.from(str)
+const jsonBuff = bufferFromString.toJSON()
+/*
+  {
+  jsonBuff: {
+    type: 'Buffer',
+    data: [
+      100, 117, 109, 109, 121,  32,
+      115, 116, 114, 105, 110, 103,
+       32, 104, 101, 114, 101,  32,
+       97
+    ]
+  }
+}
+*/ 
 console.log({jsonBuff})
+
+const otherJsonBuff = JSON.stringify(bufferFromString)
+/*
+  {
+  otherJsonBuff: '{"type":"Buffer","data":[100,117,109,109,121,32,115,116,114,105,110,103,32,104,101,114,101,32,97]}'
+}
+*/ 
+console.log({otherJsonBuff})
