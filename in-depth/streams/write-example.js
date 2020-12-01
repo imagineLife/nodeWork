e3bbebf60e5e184874e53beea5d6a2c6d1260ffb
Eric Laursen <ericjlaursen@gmail.com>
2020-12-01 05:52:59 -0500

    more

--- in-depth/streams/write-example.js
+++ in-depth/streams/write-example.js
@@ -8,3 +8,31 @@ writable.write('A\n')
 writable.write('B\n')
 writable.write('C\n')
 writable.end('nothing more to write')
+
+
+
+/*
+  Accepting More than strings && buffers
+  - streams only work with String + Buffers by default
+  - set `objectMode: true` in the Writable Constructor && now 
+    the stream can take in & write MORE types of data
+*/ 
+'use strict'
+const { Writable } = require('stream')
+const makeWriteStream = (d) => {
+  return new Writable({
+    objectMode: true,
+    write (chunk, enc, next) {
+      d.push(chunk)
+      next()
+    }
+  })
+}
+const dataArr = []
+const objModeStream = makeWriteStream(dataArr)
+const finishAndLog = () => { console.log('finished writing', dataArr) }
+objModeStream.on('finish', finishAndLog)
+objModeStream.write('A\n')
+objModeStream.write(1)
+objModeStream.write(false)
+objModeStream.end('nothing more to write')
