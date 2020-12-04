9973591d3ee4bdf0c107c5c25f3e1bf1d14f7615
Eric Laursen <ericjlaursen@gmail.com>
2020-12-04 18:46:07 -0500

    init

---
+++ in-depth/streams/piping.js
@@ -0,0 +1,9 @@
+/*
+  Piping 
+  available in bash...
+  `cat source-file | grep find-someting`
+    - pipe...
+      - reads the OUTPUT from the left 
+      - writed to the right-side command
+  same principle in node with .pipe
+*/ 
