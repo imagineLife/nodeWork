12434837a78f1ba5e169ed06c0dd84ff75eae1c0
Eric Laursen <ericjlaursen@gmail.com>
2020-12-03 20:55:09 -0500

    using finished

--- in-depth/streams/catching-end.js
+++ in-depth/streams/catching-end.js
@@ -9,4 +9,22 @@ const net = require('net');
    or a premature close event.
    https://nodejs.org/api/stream.html#stream_stream_finished_stream_options_callback
 */ 
+
 const { finished } = require('stream')
+
+net.createServer((socket) => {
+  
+  // write a heart-beat
+  const int = setInterval(() => {
+    socket.write('heart-beat')
+  });
+  
+  //write data on data
+  socket.on('data', d => socket.write(d.toString().toUpperCase()))
+
+  finished(socket, err => {
+    if(err) console.error(`socket err:`, err);
+    clearInterval(int);
+  })
+
+})
