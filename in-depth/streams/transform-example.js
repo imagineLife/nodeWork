8fc8c51c096cb644c9893bb6bcb6f48e1787c3a2
Eric Laursen <ericjlaursen@gmail.com>
2020-12-05 11:18:51 -0500

    more

--- in-depth/streams/transform-example.js
+++ in-depth/streams/transform-example.js
@@ -1,20 +1,46 @@
 'use strict'
-const { createGzip } = require('zlib')
-const encodingType = 'base64' //hex
-const timeoutVal = 500
-const gZipStream = createGzip()
-function dataHandler(data){
-  console.log('got gzip data', data.toString(encodingType))
-}
+// const { createGzip } = require('zlib')
+// const encodingType = 'base64' //hex
+// const timeoutVal = 500
+// const gZipStream = createGzip()
+// function dataHandler(data){
+//   console.log('got gzip data', data.toString(encodingType))
+// }
 
-gZipStream.on('data', dataHandler)
-gZipStream.write('first')
-setTimeout(() => {
-  gZipStream.end('second')
-}, timeoutVal)
+// gZipStream.on('data', dataHandler)
+// gZipStream.write('first')
+// setTimeout(() => {
+//   gZipStream.end('second')
+// }, timeoutVal)
 
 /*
   Running this 
   got gzip data H4sIAAAAAAAAEw==
   got gzip data S8ssKi4pTk3Oz0sBAP/7ZB0LAAAA
 */ 
+
+const { Transform } = require('stream')
+const { scrypt } = require('crypto')
+const createTransformStream = () => {
+  return new Transform({
+    decodeStrings: false,
+    encoding: 'hex',
+    transform (chunk, enc, next) {
+      scrypt(chunk, 'a-salt', 32, (err, key) => {
+        if (err) {
+          next(err)
+          return
+        }
+        next(null, key)
+      })
+    }
+  })
+}
+const transform = createTransformStream()
+transform.on('data', (data) => {
+  console.log('got data:', data)
+})
+transform.write('A\n')
+transform.write('B\n')
+transform.write('C\n')
+transform.end('nothing more to write')
