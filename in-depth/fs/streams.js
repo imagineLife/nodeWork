c1396f7bf641d9783ef1416c182248fb8d217b6f
Eric Laursen <ericjlaursen@gmail.com>
2020-12-05 14:21:34 -0500

    using a stream transformer

--- in-depth/fs/streams.js
+++ in-depth/fs/streams.js
@@ -1,15 +1,43 @@
 const { pipeline } = require('stream');
 const { join } = require('path');
 const { createReadStream, createWriteStream } = require('fs');
+const { Transform } = require('stream');
+
+function handlePipelineError(err){
+  if(err){
+    console.error(err);
+    return;
+  }
+  console.log('finished writing stream!')
+}
 
 pipeline(
   createReadStream(__filename),
   createWriteStream(join(__dirname,'stream-out.txt')),
-  err => {
-    if(err){
-      console.error(err);
-      return;
+  handlePipelineError
+)
+
+/*
+  Good for...
+  - Large-files
+  - low memory usage
+  - 
+*/ 
+
+
+// Levearging Transform stream between files
+const createUpperCaseStream = () => {
+  return new Transform({
+    transform (chunk, enc, nxt){
+      const upd = chunk.toString().toUpperCase()
+      nxt(null,upd);
     }
-    console.log('finished writing stream!')
-  }
+  })
+}
+
+pipeline(
+  createReadStream(__filename),
+  createUpperCaseStream(),
+  createWriteStream(join(__dirname, 'out-upper-stream.txt')),
+  handlePipelineError
 )
