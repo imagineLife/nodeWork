951b707e779d79c924706d95897b68e4595ce435
Eric Laursen <ericjlaursen@gmail.com>
2020-12-05 15:46:33 -0500

    some notes

--- in-depth/dirs/watching.js
+++ in-depth/dirs/watching.js
@@ -6,3 +6,19 @@ function watchHandler(e,fileName){
 }
 
 watch(PATH_TO_WATCH, watchHandler)
+
+/*
+  Triggering the watch handler
+  - open a new terminal
+    - node -e "fs.writeFileSync('test', 'test')"
+      - create new file called 'test' with text 'test'
+      - creates event "rename"
+    - node -e "fs.mkdirSync('test-dir')"
+      - creates folder test-dir
+      - creates event "rename"
+    - node -e "fs.chmodSync('test-dir', 0o644)"
+      - sets permissions on dir
+      - creates event ""rename
+    
+
+*/ 
