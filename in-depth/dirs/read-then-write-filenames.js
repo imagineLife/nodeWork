cf534b01aeb0b048ebdbc3f7368e8b5fed718536
Eric Laursen <ericjlaursen@gmail.com>
2020-12-06 12:49:38 -0500

    simpler fs-only, no streams

--- in-depth/dirs/read-then-write-filenames.js
+++ in-depth/dirs/read-then-write-filenames.js
@@ -1,31 +1,43 @@
+'use strict'
 const assert = require('assert')
 const { join } = require('path')
 const fs = require('fs')
-
-const OUTPUT_DIR = 'project'
-const OUTPUT_FILE = 'read-then-write.txt';
-
-const project = join(__dirname, OUTPUT_DIR)
+const project = join(__dirname, 'project')
 
 try { 
-  // remove the file before the process starts
-  fs.rmdirSync(project, {recursive: true}) 
-} catch (err) {
+  fs.rmdirSync(project, {recursive: true}) } 
+catch (err) {}
 
-}
-
-const knownFiles = Array.from(Array(5), () => {  
+const files = Array.from(Array(5), () => {  
   return join(project, Math.random().toString(36).slice(2))
 })
 
 fs.mkdirSync(project)
 
-for (const f of knownFiles) fs.closeSync(fs.openSync(f, 'w'))
-const out = join(__dirname, OUTPUT_FILE)
+for (const f of files) fs.closeSync(fs.openSync(f, 'w'))
+
+const out = join(__dirname, 'out.txt')
+
 function exercise () {  
-  // TODO read the knownFiles in the project folder  
+  // TODO read the files in the project folder  
   // and write them to the out.txt file
+  
+  // 1. Make readable stream, reading from directory
+  function readDirCallback(err, data){
+    if(err) console.log('ERROR READING!', err)
+    let resString = data.reduce((acc, curItm, curIdx) => {
+      if (curIdx < data.length - 1) acc += `${curItm},`
+        acc += curItm
+        return acc
+    }, '')
+    
+    fs.writeFile(out, resString, (err) => {
+      if(err) console.log('ERROR WRITING!')
+      return;
+    })
+  }
+  fs.readdir(project, readDirCallback)
 }
 exercise()
-assert(fs.readFileSync(out).toString(), knownFiles.toString())
+assert(fs.readFileSync(out).toString(), files.toString())
 console.log('passed!')
