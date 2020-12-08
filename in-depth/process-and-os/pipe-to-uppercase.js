1497ebf650088c0dda5cc4ce3c0c74ae7fb4cd66
Eric Laursen <ericjlaursen@gmail.com>
2020-12-07 20:04:20 -0500

    more notes

--- in-depth/process-and-os/pipe-to-uppercase.js
+++ in-depth/process-and-os/pipe-to-uppercase.js
@@ -1,4 +1,6 @@
 console.log('initialized');
+console.log(process.stdin.isTTY ? 'terminal' : 'piped to');
+
 const { Transform } = require('stream');
 const createTransStream = () => {
   return new Transform({
@@ -13,3 +15,19 @@ const ucStream = createTransStream()
 process.stdin
   .pipe(ucStream)
   .pipe(process.stdout)
+
+
+/*
+  2 interesting ways to use this!
+  - run 
+  node - "crypto.randomBytes(100).toString('hex')" | node pipe-to-uppercase.js
+    - this will...
+      - run the randomBytes str
+      - pipe the res to this file
+      - output the uppercase version to stdout
+  node pipe-to-uppercase
+    - this will...
+      - start a node server
+      - allow text input in the cmd line
+      - convert any text input in cmd line to UPPERCASE
+*/ 
