0e8cd60f8a283e410d5bec5e2776ddd4e8510afc
Eric Laursen <ericjlaursen@gmail.com>
2020-12-12 11:24:22 -0500

    repeating output

--- in-depth/process-and-os/system-info.js
+++ in-depth/process-and-os/system-info.js
@@ -4,7 +4,10 @@ const { hostname,
   homedir, 
   tmpdir, 
   platform,
-  type
+  type,
+  uptime,
+  freemem,
+  totalmem
 } = require('os');
 
 console.log({
@@ -12,15 +15,28 @@ console.log({
   homedir: homedir(), 
   tmpdir: tmpdir(),
   platform: platform(),
-  type: type()
+  type: type(),
+  uptime: uptime(),
+  freemem: freemem(),
+  totalmem: totalmem()
 })
 
+function logSysStats(){
+  console.log({
+    uptime: uptime(),
+    freemem: freemem(),
+    totalmem: totalmem()
+  })
+}
+
 /*
-  Output
+  Output of above
   {
     hostname: 'Jakes-3.local',
     homedir: '/Users/Jake',
-    tmpdir: '/var/folders/2-letters/random-string/Letter'
+    tmpdir: '/var/folders/2-letters/random-string/Letter',
+    platform: 'darwin',
+    type: 'Darwin'
   }
 
   hostname 
@@ -30,4 +46,24 @@ console.log({
   type
     uses uname on non-windows
     uses ver   on windows
+
+
+    System STATS
+    - uptime
+    - free memory
+    - total memory
 */
+
+
+setInterval(logSysStats, 1000)
+/** 
+  Output of interval
+  { uptime: 603410, freemem: 840437760, totalmem: 17179869184 }
+  { uptime: 603411, freemem: 840163328, totalmem: 17179869184 }
+  { uptime: 603412, freemem: 840929280, totalmem: 17179869184 }
+  { uptime: 603413, freemem: 840273920, totalmem: 17179869184 }
+  { uptime: 603414, freemem: 839954432, totalmem: 17179869184 }
+  { uptime: 603415, freemem: 843300864, totalmem: 17179869184 }
+  { uptime: 603416, freemem: 843079680, totalmem: 17179869184 }
+  { uptime: 603417, freemem: 843321344, totalmem: 17179869184 }
+*/ 
