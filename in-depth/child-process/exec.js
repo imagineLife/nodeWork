69acddefec1cc072dd277b296b31ef5b061e5fe4
Eric Laursen <ericjlaursen@gmail.com>
2020-12-13 18:50:04 -0500

    another command

--- in-depth/child-process/exec.js
+++ in-depth/child-process/exec.js
@@ -3,10 +3,13 @@
   execSync
     - returns a buffer, containing the output of the command
     - both stdout && stderr output
+    - throws error on non-zero exit code, so 
+      wrap in a try/catch
 */ 
 
 const { execSync } = require('child_process');
 
+// FIRST COMMAND
 /*
   replace the `node` keyword with process.execPath
   - ensures that the subprocess will be
@@ -16,15 +19,27 @@ const { execSync } = require('child_process');
 // node version
 // const RUNNABLE_PROCESS = `node -e "console.error('subprocess stdio output')"`
 // 
-const RUNNABLE_PROCESS = `${process.execPath} -e "console.error('subprocess stdio output')"`
-const LIST_FILES = process.platform === 'win32' ? 'dir' : 'ls'
+// const RUNNABLE_PROCESS = `${process.execPath} -e "console.error('subprocess stdio output')"`
+// const LIST_FILES = process.platform === 'win32' ? 'dir' : 'ls'
 
-const action = execSync(RUNNABLE_PROCESS) //RUNNABLE_PROCESS LIST_FILES
+// const action = execSync(RUNNABLE_PROCESS) //RUNNABLE_PROCESS LIST_FILES
 
-console.log(action.toString())
+// console.log(action.toString())
+
 
 /*
-  Running this will output..
+  Running the above will output..
   "subprocess stdio output"
 */ 
 
+
+
+// SECOND COMMAND
+/*
+
+*/
+try {
+  execSync(`${process.execPath} -e "process.exit(1)"`)
+} catch (err) {
+  console.error('CAUGHT ERROR:', err)
+}
