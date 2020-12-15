c174d54bb1665ca1383e8ac246e0e7a1c4488e21
Eric Laursen <ericjlaursen@gmail.com>
2020-12-15 06:20:13 -0500

    notes

--- in-depth/child-process/child-stdio.js
+++ in-depth/child-process/child-stdio.js
@@ -9,7 +9,7 @@
 const { spawn } = require('child_process');
 const NODE_FLAG = `-e`
 const RUNNABLE_STRING = `console.error('child-process error output');process.stdin.pipe(process.stdout)`;
-const PARENT_STRING_INPUT = 'parent input string\n'
+const STRING_INPUT = 'string input from parent here\n'
 const spawnRes = spawn(
   process.execPath,
   [
@@ -21,7 +21,7 @@ const spawnRes = spawn(
 
 spawnRes.stdout.pipe(process.stdout)
 spawnRes.stderr.pipe(process.stdout)
-spawnRes.stdin.write(PARENT_STRING_INPUT)
+spawnRes.stdin.write(STRING_INPUT)
 spawnRes.stdin.end()
 
 /*
@@ -31,5 +31,33 @@ spawnRes.stdin.end()
     child-process error output
     parent input string
   
+  RUNNABLE_STRING
+    - logs an error
+    - intended to pipe child process stdin to the child stdout
   
+  NOTICE the options object
+    - stdio as arr of 3 strings, pipe
+    - THIS IS 'DEFAULT' BEHAVIOR, set explicitly here
+    - pipe means to expose a stream for each of 3 stdio devices
+    - each element 'corresponds with the file-descriptors'
+      - first element sets child-process STDIN
+      - second element sets child-process STDOUT
+      - third element sets child-process STDERR
+  
+  PROCESS OVERVIEW
+  - build a few vars, including runnable-string (see above)
+  - spawn a child process
+    - using node binary (process.execPath)
+    - passing -e evaluate flag
+    - passing runnable string as executable line to spawn process
+      - this pipes the child-process stdin to the stdout
+    - passes the config object (see above)
+  - piping...
+    - pipe child out to parent out
+    - pipe child err to parent out
+  - writing...
+    - write to child input stream
+  - exit the child
+    - triggers the parent to exit
+
 */ 
