33ac6e56bc37eba674c8fd070cf8dd7e397ae3ef
Eric Laursen <ericjlaursen@gmail.com>
2020-12-15 19:33:26 -0500

    notes

--- in-depth/child-process/child-stdio-spawnSync.js
+++ in-depth/child-process/child-stdio-spawnSync.js
@@ -16,3 +16,21 @@ spawnSync(
     stdio: IO_ARR
   }
 )
+
+/*
+  Running the above returns...
+    this is a string set in parent
+  
+  - spawns a child process in sync mode
+  - leverages the absolute path to node process
+  - leverages -e node flag, evaluate as js
+  - passes a child process
+    - log an error
+    - pipe child input to parent output
+  - pass config to child process
+    - pass input to child process
+    - pass stdio arr
+      - pipe std input
+      - inherit std output
+      - ignore std err
+*/ 
