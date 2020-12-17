88199001d175d2b9915a9e83b2a09cc01b0e5215
Eric Laursen <ericjlaursen@gmail.com>
2020-12-17 06:51:24 -0500

    strict

--- in-depth/unit-tests/add.js
+++ in-depth/unit-tests/add.js
@@ -1,14 +1,20 @@
-const { equal, strictEqual } = require('assert')
+const { equal, strictEqual, strict } = require('assert')
+
+/*
+  NOTE
+  strict can 'wrap' nonStrict assertions and make the assertion type-assure
+*/
 
 // Fn Def
 function add (a,b){ return a + b }
 
 // Passes
 equal(add(2,2), 4); 
-strictEqual(add(3,5),8
+strictEqual(add(3,5),8)
 
 // Fails
 const thisRes = add(2,3);
 const stringVal = '5';
-// equal('number', typeof stringVal)
+equal('number', typeof stringVal)
+strict.equal('number', typeof stringVal)
 // returns AssertionError [ERR_ASSERTION]: 'number' == 'string'
