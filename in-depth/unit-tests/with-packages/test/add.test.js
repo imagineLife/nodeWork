b67ae91fbfb5cffe68d63bc1949729fb4ce64ec1
Eric Laursen <ericjlaursen@gmail.com>
2020-12-19 07:37:04 -0500

    records the output

--- in-depth/unit-tests/with-packages/test/add.test.js
+++ in-depth/unit-tests/with-packages/test/add.test.js
@@ -21,4 +21,25 @@ test('adds two numbers', async ({ equal }) => {
   NOTICE
   the async param gets destructured
   - the async param is the assert library
+
+
+  Run the above with node add.test.js
+  RETURNS...
+    TAP version 13
+    # Subtest: throw when inputs are not numbers
+        ok 1 - expected to throw
+        ok 2 - expected to throw: Error inputs must be numbers
+        ok 3 - expected to throw: Error inputs must be numbers
+        ok 4 - expected to throw: Error inputs must be numbers
+        1..4
+    ok 1 - throw when inputs are not numbers # time=23.526ms
+
+    # Subtest: adds two numbers
+        ok 1 - should be equal
+        ok 2 - should be equal
+        1..2
+    ok 2 - adds two numbers # time=2.554ms
+
+    1..2
+    # time=39.644ms
 */ 
