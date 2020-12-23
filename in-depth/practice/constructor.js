b2e75fca308e5d51ad3dcf4b4d6e5ccaa69ad365
Eric Laursen <ericjlaursen@gmail.com>
2020-12-23 05:47:02 -0500

    more comments

--- in-depth/practice/constructor.js
+++ in-depth/practice/constructor.js
@@ -1,9 +1,15 @@
 /*
+  Constructor Functions
+  here, Wolf && Dog are constructor function
+ */ 
+
+/*
   Using PascaleCase for fns that are
    intended to be called with `new` keyword
 */ 
 
 // the prototype of Wolf is Object.prototype
+// logging `this` returns Wolf {}
 function Wolf (name){
   this.name = name;
 }
@@ -14,7 +20,17 @@ Wolf.prototype.yowl = function(){
 }
 
 // the prototype of Dog is wolf
+// logging `this` returns Wolf {}
 function Dog(name){
+
+  /* 
+    - `this` gets passed to wolf.call
+    - `this` is referenced inside the wolf constructor
+      && assigns this.name from the name
+    - the 2nd `param` of the call fn is the fn param
+      - the "name" arg passed to Wolf is Dolly the dog,
+        && Dolly the dog gets assigned to this.name
+  */
   Wolf.call(this, `${name} the dog`)
 }
 
