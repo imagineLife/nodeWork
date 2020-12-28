3f2f0221985d2c070dbb3751215530abcb2697ad
Eric Laursen <ericjlaursen@gmail.com>
2020-12-27 20:50:07 -0500

    comment on how it gets un-sugared

--- in-depth/practice/classes.js
+++ in-depth/practice/classes.js
@@ -9,6 +9,17 @@ class Vehicle {
   honk(){ console.log(`${this.name}: HONK HONK`) }
 }
 
+/*
+  the above `Vehicle` class gets de-composed, un-'sugar'ed to..
+  function Wolf (name){
+    this.name = name
+  }
+
+  Wolf.prototype.howl = function(){
+    console.log(`${this.name}: HONK HONK`)
+  }
+*/ 
+
 class SportsCar extends Vehicle{
   constructor(name){
     super(`${name} the SportsCar`)
