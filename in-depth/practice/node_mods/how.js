c21b24c4f2210350fd225b9bc6155cd014a3e572
Eric Laursen <ericjlaursen@gmail.com>
2020-12-30 05:54:33 -0500

    ways of installing packages

---
+++ in-depth/practice/node_mods/how.js
@@ -0,0 +1,18 @@
+/*
+  Installing Packages && version numbers
+
+  less-than a verson
+    npm install react@"<16"
+
+  within a range
+    npm install react">=15 <16"
+
+  a specific version
+    npm install react@15.0.0
+  
+  include patches
+    npm install react@15.0.*
+
+  include minor releases
+    npm install react@15
+*/ 
