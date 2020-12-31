945400f83017f503760d9b1512e33c6badb49faa
Eric Laursen <ericjlaursen@gmail.com>
2020-12-31 11:14:54 -0500

    passes

--- in-depth/practice/event-once.js
+++ in-depth/practice/event-once.js
@@ -14,5 +14,39 @@
 
 */ 
 
+const {equal} = require('assert');
 const { EventEmitter } = require('events');
-let count = 0;
+let gloablCount = 0;
+
+const e = new EventEmitter()
+
+let TICK_EMIT_TIME = 100
+let ASSERT_TIME = 250
+
+function emitATick(){
+  e.emit('tick')
+}
+
+/*
+  TRIED this as the fn in the setTimeout listener
+  the `this` assertion failed
+*/ 
+// function asserter(){
+//   equal(gloablCount, 1)
+//   equal(this, e)
+//   console.log('passed!')
+// }
+
+function listener(){
+  gloablCount++
+  setTimeout(() => {
+    equal(gloablCount, 1)
+    equal(this, e)
+    console.log('passed!')
+  }, ASSERT_TIME)
+}
+
+setInterval(emitATick,TICK_EMIT_TIME)
+
+e.once('tick', listener)
+
