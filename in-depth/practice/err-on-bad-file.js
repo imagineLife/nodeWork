f8fe981a036a1834c05460e3617480190b539499
Eric Laursen <ericjlaursen@gmail.com>
2021-01-02 11:27:03 -0500

    throw err on a bad file read

---
+++ in-depth/practice/err-on-bad-file.js
@@ -0,0 +1,27 @@
+const fs = require('fs')
+const assert = require('assert')
+
+async function read (file) {
+  try{
+    const content = await fs.promises.readFile(file)
+    return content
+  }catch(e){
+    throw Error('failed to read')
+  }
+}
+
+async function check () {
+  
+  await assert.rejects(
+    read('not-a-valid-filepath'),
+    new Error('failed to read')
+  )
+
+  assert.deepEqual(    
+    await read(__filename),    
+    fs.readFileSync(__filename)  
+  )  
+  
+  console.log('passed!')
+  
+}check()
