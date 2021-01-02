79e4981bed903d9258f4ab000e2eee92ad0e00f7
Eric Laursen <ericjlaursen@gmail.com>
2021-01-02 11:52:22 -0500

    init

---
+++ in-depth/practice/make-upercase-transform-stream.js
@@ -0,0 +1,43 @@
+const { 
+  Readable, 
+  Writable, 
+  Transform, 
+  PassThrough, 
+  pipeline 
+} = require('stream')
+
+const assert = require('assert')
+const createWritable = () => {  
+  const sink = []  
+  const writable = new Writable({    
+    write(chunk, enc, cb) {      
+      sink.push(chunk.toString())      
+      cb()    
+    }  
+  })  
+  
+  writable.sink = sink  
+  return writable
+}
+const readable = Readable.from(['a', 'b', 'c'])
+const writable = createWritable()
+
+// TODO: replace the pass through stream
+// with a transform stream that uppercases
+// incoming characters
+// const transform = new PassThrough()
+const transChunk = chunk => chunk.toString().toUpperCase();
+const transform = new Transform({
+		// MUST set this as the content passed to the readable, below, is an arr
+		objectMode: true,
+		// call transform on the incoming chunks
+    transform (chunk, enc, next) {
+			const transformedChunk = transChunk(chunk)
+        next(null, transformedChunk)
+    }
+})
+pipeline(readable, transform, writable, (err) => {  
+  assert.ifError(err)  
+  assert.deepStrictEqual(writable.sink, ['A', 'B', 'C'])
+  console.log('passed!')
+})
