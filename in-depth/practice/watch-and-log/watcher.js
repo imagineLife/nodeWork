fdcff8aacac3824a2e801fd6d3c9615870974071
Eric Laursen <ericjlaursen@gmail.com>
2021-01-10 20:12:43 -0500

    get args && apply to cur file

--- in-depth/practice/watch-and-log/watcher.js
+++ in-depth/practice/watch-and-log/watcher.js
@@ -11,4 +11,27 @@ let LOG_FILE = __filename;
 // console.log({LOG_FILE})
 // console.log({WATCH_DIR})
 
+function applyArgToCurDir(str){
+  let argVal = str.substring(6);
+    return `${__dirname}/${argVal}`
+}
+
+if(args.length > 2){
+  args.forEach((a, argIdx) => {
+    // skip default args
+    if(argIdx < 2) return;
+
+    if(a.includes(`--DIR`)){
+      WATCH_DIR = applyArgToCurDir(a)
+    }
+
+    if(a.includes(`--LOG`)){
+      LOG_FILE = applyArgToCurDir(a)
+    }
+  })
+
+  console.log({WATCH_DIR})
+  console.log({LOG_FILE})
+  
+}
 
