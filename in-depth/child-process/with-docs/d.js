a3525ba949eb153a536b9ce2f9f5a3c810c9f5a9
Eric Laursen <ericjlaursen@gmail.com>
2021-01-19 20:12:31 -0500

    exec nonSysnc

---
+++ in-depth/child-process/with-docs/d.js
@@ -0,0 +1,13 @@
+const { exec } = require('child_process');
+
+const RUNNABLE_STRING = "console.log('A');console.error('B')"
+
+const CHILD_PROCESS_STR = `${process.execPath} -e "${RUNNABLE_STRING}"`
+
+function childProcessCallback(err,stdout,stderr){
+  console.log('err',err)
+  console.log('stdout: ',stdout);
+  console.log('stderr: ', stderr)
+}
+
+exec(CHILD_PROCESS_STR, childProcessCallback)
