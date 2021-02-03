ca3015211d71b9960d1e71a35321e509517cc3a3
Eric Laursen <ericjlaursen@gmail.com>
2021-02-02 20:40:26 -0500

    example of waiting till timeout ends

--- in-depth/child-process/with-docs/2/f3.js
+++ in-depth/child-process/with-docs/2/f3.js
@@ -1,5 +1,5 @@
 const { spawnSync } = require('child_process');
 
-const RUN_STR = `process.exit(1)`
+const RUN_STR = `setTimeout(() => { console.log('done?')}, 1200)`
 const res = spawnSync(process.execPath, ['-e', RUN_STR]);
-console.log(res)
+console.log(res.stdout.toString())
