86c0849f1f0d41d31c73b016c77962fb3f3e90d8
Eric Laursen <ericjlaursen@gmail.com>
2021-02-15 07:17:24 -0500

    example

--- in-depth/process-and-os/piping-from-cli-to-file/to-file-3x.js
+++ in-depth/process-and-os/piping-from-cli-to-file/to-file-3x.js
@@ -1,3 +1,13 @@
+/*
+  run with 
+  node -p "crypto.randomBytes(25).toString('hex')" | node to-file-3x.js > out.txt 2> err.txt
+  will...
+  - create random hex'd crypto string
+  - pipe string to the `to-file-3x` node process
+  - pipe output to out.txt
+  - pipe err output to err.txt
+*/ 
+
 const { Transform } = require('stream')
 
 function makeUpperStream(){
