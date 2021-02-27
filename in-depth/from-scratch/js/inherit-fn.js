efc6f478b0ce4a42e8e6aeab53bf1814761712b3
Eric Laursen <ericjlaursen@gmail.com>
2021-02-27 08:49:17 -0500

    adding fn to child obj

--- in-depth/from-scratch/js/inherit-fn.js
+++ in-depth/from-scratch/js/inherit-fn.js
@@ -3,13 +3,24 @@ const livingSpace = {
   beds: 1,
   rooms: 1,
   walls: 4,
-  bathrooms: 1
+  bathrooms: 1,
+  countRooms: function(){ return this.rooms + this.bathrooms}
+  // countWindows: function(){ return this.countRooms() * this.windowsPerRoom }
 }
 
 const House = Object.create(livingSpace, {
   doors: { value: 2 },
   carpets: { value:  1 },
   rooms: { value:  5 },
-  bathrooms: { value:  1.5 }
+  bathrooms: { value:  1.5 },
+  windowsPerRoom: { value: 2 }
 })
-console.log(House[process.argv[2]])
+
+/*
+
+*/
+House.countWindows = function(){ return this.countRooms() * this.windowsPerRoom };
+
+let cliVar = process.argv[2]
+console.log(cliVar)
+console.log(typeof House[cliVar] !== 'function' ? House[cliVar] : House[cliVar]())
