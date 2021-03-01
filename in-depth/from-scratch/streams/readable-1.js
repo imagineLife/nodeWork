143353361fc11f32f5b471cf41dd7a6d87848a31
Eric Laursen <ericjlaursen@gmail.com>
2021-03-01 05:49:15 -0500

    notes

--- in-depth/from-scratch/streams/readable-1.js
+++ in-depth/from-scratch/streams/readable-1.js
@@ -1,6 +1,35 @@
 const { Readable } = require('stream');
 
-let sourceArr = ['abc','this is a long string here', 'animal', ['itm', 'itm2','itm23'].toString()]
+/*
+  Encoding
+  - only works on string inputs
+
+  objectMode
+  - can parse js content, without encoding being set
+*/ 
+
+
+/*
+  WITHOUT encoding set, this will succeed
+  - without encoding, sends buffers to console
+  - with encoding set, sends parsed content to console
+*/
+let sourceArr = [
+  'abc',
+  'this is a long string here', 
+  '1', 
+  ['itm', 'itm2','itm23'].toString()
+]
+
+/*
+  WITH objectMode set, this will succeed
+*/ 
+// let sourceArr = [
+//   'abc',
+//   'this is a long string here', 
+//   1, 
+//   ['itm', 'itm2','itm23']
+// ]
 
 function dataHandler(d){
   console.log(`got data`)
@@ -10,14 +39,16 @@ function dataHandler(d){
 
 function makeReadStream(){
   return new Readable({
+    encoding: `utf8`,
+    // objectMode: true,
     read(size){
-      console.log(`READING fn, size ->`, size)
+      // console.log(`READING fn, size ->`, size)
       if(sourceArr.length === 0 ){
         this.push(null)
       }else{
-        console.log('has length')
-        console.log('sourceArr')
-        console.log(sourceArr)
+        // console.log('has length')
+        // console.log('sourceArr')
+        // console.log(sourceArr)
         this.push(sourceArr.shift())
       }
     }
