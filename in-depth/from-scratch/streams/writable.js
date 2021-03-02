bdaf0db1bdaae42ddb3f5c9f1d8b5049cac533c9
Eric Laursen <ericjlaursen@gmail.com>
2021-03-01 19:52:33 -0500

    must call end

---
+++ in-depth/from-scratch/streams/writable.js
@@ -0,0 +1,22 @@
+const { Writable } = require('stream');
+
+let resArr = [];
+
+const mws = (d) => {
+  return new Writable({
+    objectMode: true,
+    write(ch,enc,cb){
+      d.push(ch)
+      cb()
+    }
+  })
+}
+
+const ws = mws(resArr)
+
+ws.on('finish', () => {console.log('finished?! ', resArr)})
+ws.write('asdf')
+ws.write(1234)
+ws.write(false)
+ws.write([1,2,3])
+ws.end('MUST CALL END!')
