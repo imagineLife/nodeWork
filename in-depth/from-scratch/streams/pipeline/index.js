fe63e1ab3d28ce5acdbc889a09ad7e29a2319e7c
Eric Laursen <ericjlaursen@gmail.com>
2021-03-03 06:37:48 -0500

    zip && unzip options

--- in-depth/from-scratch/streams/pipeline/index.js
+++ in-depth/from-scratch/streams/pipeline/index.js
@@ -1,4 +1,8 @@
 /*
+
+  REQUIRES
+  a sibling `txt.txt` file, containing text
+
   Leverage the pipeline api
   - build a readable stream from a file `incoming.txt`
   - in the file put 4 paragraphs of lorem ipsum from [a generator](https://www.lipsum.com/)
@@ -10,15 +14,28 @@ const {
   createReadStream, 
   createWriteStream 
 } = require('fs');
-const { pipeline, Transform } = require('stream');
+
+const { 
+  pipeline, 
+  Transform 
+} = require('stream');
+
+const { createGzip, createGunzip } = require('zlib');
 
 const IN_DOC = `txt.txt`
+const SMALL_IN_DOC = `txt-small.txt`
 const OUT_DOC = `outgoing.txt`
-const readStr = createReadStream(`./${IN_DOC}`)
-const writeStr = createWriteStream(`./${OUT_DOC}`)
+const SMALL_OUT_DOC = `outgoing_small.txt`
+
+// const txtReadStream = createReadStream(`./${IN_DOC}`)
+// const writeStr = createWriteStream(`./${OUT_DOC}`)
+
+const readWriteStr = createReadStream(`./${OUT_DOC}`)
+const writeDeCompressedFile = createWriteStream(`./${SMALL_OUT_DOC}`)
+
 
 // DIRECT pipeline, from input to output
-// pipeline(readStr, writeStr, (err,resVal)=>{
+// pipeline(txtReadStream, writeStr, (err,resVal)=>{
 //   if(err) console.log(`err `,err)
 //   console.log('no err!')
 //   console.log(resVal)
@@ -26,18 +43,47 @@ const writeStr = createWriteStream(`./${OUT_DOC}`)
 
 // Pipeline including Transform stream
 
-const mts = ()=> {
-  return new Transform({
-    transform(chunk, encoding, callback) {
-      let upper = chunk.toString().toUpperCase()
-      return callback(null,upper)
-    }
-  })
-}
+// const mts = ()=> {
+//   return new Transform({
+//     transform(chunk, encoding, callback) {
+//       let upper = chunk.toString().toUpperCase()
+//       return callback(null,upper)
+//     }
+//   })
+// }
 
-const upperStream = mts()
+// const upperStream = mts()
 
-pipeline(readStr, upperStream, writeStr, (err,resVal)=>{
-  if(err) console.log(`err `,err)
-  console.log('Done! see outgoing.txt for the transformed stream')
+// pipeline(txtReadStream, upperStream, writeStr, (err,resVal)=>{
+//   if(err) console.log(`err `,err)
+//   console.log('Done! see outgoing.txt for the transformed stream')
+// });
+
+
+
+
+// Pipeline including Compression
+// const zipStream = createGzip()
+
+// pipeline(txtReadStream, zipStream, writeStr, (err,resVal)=>{
+//   if(err) console.log(`err `,err)
+//   console.log('Done! see outgoing.txt for the transformed stream')
+// });
+
+const unzipStream = createGunzip()
+
+// NO DICE
+// try{
+//   readWriteStr.pipe(unzipStream).pipe(writeDeCompressedFile) 
+// }catch(e){
+//   console.log('caught error')
+//   console.log(e)
+// }
+
+pipeline(readWriteStr, unzipStream, writeDeCompressedFile, (err,resVal)=>{
+  if(err){
+    console.log(`err `,err)
+  }else{
+    console.log('Done! see outgoing.txt for the transformed stream')
+  }
 });
