38645d5e74687ff3eed4b298bf7a523049525c77
Eric Laursen <ericjlaursen@gmail.com>
2021-03-04 20:14:03 -0500

    2 versions outputting to a string

--- in-depth/from-scratch/fs/reading.js
+++ in-depth/from-scratch/fs/reading.js
@@ -4,4 +4,13 @@ const { join } = require('path');
 const OTHER_FILE = join(__dirname,`global-vars.js`)
 
 // output to buffer
-let fileContent = readFileSync(OTHER_FILE)
+// let fileContent = readFileSync(OTHER_FILE)
+
+// to string v1
+// const STRING_VERSION = fileContent.toString()
+
+// to String v2
+let fileContent = readFileSync(OTHER_FILE, { encoding: 'utf8'});
+const STRING_VERSION = fileContent
+console.log('STRING_VERSION')
+console.log(STRING_VERSION)
