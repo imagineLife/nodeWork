e5855be195677b360ff0dccc258949ee9dda23ed
Eric Laursen <ericjlaursen@gmail.com>
2021-03-04 19:57:25 -0500

    a cross-platform-path

--- in-depth/from-scratch/fs/global-vars.js
+++ in-depth/from-scratch/fs/global-vars.js
@@ -1,2 +1,8 @@
 console.log({filename: __filename});
 console.log({dir: __dirname})
+
+// a cross-platform path
+const A_FILE_NAME = 'water.js'
+const crossPlatformPath = require('path').join(__dirname, A_FILE_NAME)
+console.log('crossPlatformPath')
+console.log(crossPlatformPath)
