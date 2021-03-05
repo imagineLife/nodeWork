6e11ad1d1f6b6fba00c471479767131138767d90
Eric Laursen <ericjlaursen@gmail.com>
2021-03-04 20:03:44 -0500

    relative between 2 paths

--- in-depth/from-scratch/fs/global-vars.js
+++ in-depth/from-scratch/fs/global-vars.js
@@ -1,8 +1,18 @@
 console.log({filename: __filename});
 console.log({dir: __dirname})
 
+const { join, relative } = require('path')
 // a cross-platform path
 const A_FILE_NAME = 'water.js'
-const crossPlatformPath = require('path').join(__dirname, A_FILE_NAME)
+const crossPlatformPath = join(__dirname, A_FILE_NAME)
 console.log('crossPlatformPath')
 console.log(crossPlatformPath)
+
+// difference between 2 paths
+
+const PATH_ONE = join(__filename)
+const PATH_TWO = join(__dirname,'water/sticks/stones/md.md')
+
+const diff = relative(PATH_ONE, PATH_TWO)
+console.log({diff})
+
