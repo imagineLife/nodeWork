93769f90b718a6188ef48e36723739db35d403f4
Eric Laursen <ericjlaursen@gmail.com>
2021-03-06 10:28:08 -0500

    init

---
+++ in-depth/from-scratch/fs/watching.js
@@ -0,0 +1,9 @@
+/*
+- watch a file system
+  - wathcing a directory
+  - maintaining state of the files in the dir
+  - logging when...
+    - a new file is added
+    - a file is deleted
+    - a file is updated
+*/ 
