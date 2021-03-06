82a207d85cd0fc35e6d93b93de4f7fcdc9da3634
Eric Laursen <ericjlaursen@gmail.com>
2021-03-05 19:22:05 -0500

    logs how many chunks were parsed

--- in-depth/from-scratch/fs/reading/with-stream.js
+++ in-depth/from-scratch/fs/reading/with-stream.js
@@ -3,8 +3,10 @@ const { createReadStream } = require('fs')
 const FILE_PATH = './demo-file.txt';
 
 const fileStream = createReadStream(FILE_PATH, {encoding: 'utf8'})
+let chunks = 1;
 fileStream.on('data', d => {
-  console.log(d)
+  console.log(chunks)
+  chunks++;
 })
 
 fileStream.on('close', d => console.log('closed'))
