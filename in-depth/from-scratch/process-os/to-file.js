102f1b0c653aeecf917ef87cd733167989a079df
Eric Laursen <ericjlaursen@gmail.com>
2021-03-07 14:53:57 -0500

    includes process-call detection

--- in-depth/from-scratch/process-os/to-file.js
+++ in-depth/from-scratch/process-os/to-file.js
@@ -3,4 +3,5 @@
   - try `node -p "4 * 2" | node to-file`
 */ 
 console.log(`to-file called, piping!!`)
+console.log(process.stdin.isTTY ? 'from terminal' : 'piped to')
 process.stdin.pipe(process.stdout)
