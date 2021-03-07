a3d51713177662a68bef647308eb29d71cc4b1e7
Eric Laursen <ericjlaursen@gmail.com>
2021-03-07 14:54:31 -0500

    comment

--- in-depth/from-scratch/process-os/to-file.js
+++ in-depth/from-scratch/process-os/to-file.js
@@ -1,6 +1,7 @@
 /*
   USING THIS
   - try `node -p "4 * 2" | node to-file`
+  - includes detecting wether the process is called directly from cli or from a piped process
 */ 
 console.log(`to-file called, piping!!`)
 console.log(process.stdin.isTTY ? 'from terminal' : 'piped to')
