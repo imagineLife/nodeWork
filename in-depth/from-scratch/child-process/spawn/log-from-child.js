3636bd31e96335eb869d3981fbb1099e2d658487
Eric Laursen <ericjlaursen@gmail.com>
2021-03-07 17:57:06 -0500

    first item

---
+++ in-depth/from-scratch/child-process/spawn/log-from-child.js
@@ -0,0 +1,19 @@
+/*
+  - spawn a child process that exits with a non-zero exit code
+  - pipe the sub-process stdout to the parent process stdout 
+    && log a string in the subprocess stdout 
+    (_the log should appear in the parent process stdout_)
+*/ 
+
+const { spawnSync } = require('child_process');
+
+// first bullet
+console.log('--start--')
+const chPro = spawnSync(process.execPath, [
+  '-e',
+  'process.exit(1)'
+])
+console.log('chPro')
+console.log(chPro)
+
+console.log('--end--')
