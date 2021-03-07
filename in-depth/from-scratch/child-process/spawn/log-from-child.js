ed4e9b0d536a12b2c996b1177527f613170e7c21
Eric Laursen <ericjlaursen@gmail.com>
2021-03-07 18:00:41 -0500

    pipe child process output to parent

--- in-depth/from-scratch/child-process/spawn/log-from-child.js
+++ in-depth/from-scratch/child-process/spawn/log-from-child.js
@@ -8,12 +8,36 @@
 const { spawnSync } = require('child_process');
 
 // first bullet
+// console.log('--start--')
+// const chPro = spawnSync(process.execPath, [
+//   '-e',
+//   'process.exit(1)'
+// ])
+// console.log('chPro')
+// console.log(chPro)
+
+// console.log('--end--')
+
+
+
+
+
+/* Second Bullt
+  - pipe the sub-process stdout to the parent process stdout 
+    && log a string in the subprocess stdout 
+    (_the log should appear in the parent process stdout_)
+*/
 console.log('--start--')
 const chPro = spawnSync(process.execPath, [
   '-e',
-  'process.exit(1)'
-])
-console.log('chPro')
-console.log(chPro)
+  'console.log("child string here")'
+],
+{
+  stdio:['pipe','inherit','inherit']
+});
+// console.log('chPro')
+// console.log(chPro)
+
+// chPro.stdout.pipe(process.stdout)
 
 console.log('--end--')
