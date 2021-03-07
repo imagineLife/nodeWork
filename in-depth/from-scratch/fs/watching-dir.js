c26171e51c6274515a2d84ac9704658bff08820a
Eric Laursen <ericjlaursen@gmail.com>
2021-03-07 14:33:52 -0500

    more logging

--- in-depth/from-scratch/fs/watching-dir.js
+++ in-depth/from-scratch/fs/watching-dir.js
@@ -8,6 +8,45 @@
     - a file is updated
 */ 
 
-const { watch } = require('fs');
+const { watch, promises: { stat : promiseStat } } = require('fs');
+
+function eventFromStats(
+  {birthtimeMs, ctimeMs, mtimeMs, atimeMs},
+  watchTime, optionalMemoryTime
+  ){
+    const stats = { 
+      birthtimeMs, ctimeMs, mtimeMs, atimeMs
+    }
+    console.log('stats')
+    console.log(stats)
+    console.log('optionalMemoryTime')
+    console.log(optionalMemoryTime)
+    console.log('// - - - - - //')
+    
+  // CREATED
+  if(birthtimeMs === ctimeMs) return 'created'
+  else return 'other'
+}
 
 const DIR_STR_TO_WATCH = './watching'
+
+// Storing global memory of file events && their event-time,
+//   attempting to remove duplicate events
+const fileEventMemory = {}
+
+async function watchHandler(ev,file){
+  console.log('---START WATCH---')
+  let now = new Date()
+  const FILE_PATH = `${DIR_STR_TO_WATCH}/${file}`
+  try{
+    const fileStats = await promiseStat(FILE_PATH)
+  const eventType = eventFromStats(fileStats, now, fileEventMemory[FILE_PATH])
+    console.log('eventType')
+    console.log(eventType)
+  }catch(e){
+    console.log('e')
+    console.log(e) 
+  }
+}
+// WATCH!
+watch(DIR_STR_TO_WATCH, watchHandler)
