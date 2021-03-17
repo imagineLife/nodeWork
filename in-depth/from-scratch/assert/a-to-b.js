70b95d2f15de833b22bab00373d48678d133d2eb
Eric Laursen <ericjlaursen@gmail.com>
2021-03-17 19:42:43 -0400

    more equality examples

--- in-depth/from-scratch/assert/a-to-b.js
+++ in-depth/from-scratch/assert/a-to-b.js
@@ -1,12 +1,21 @@
 // Shallow coercive equality
-const { equal } = require('assert');
+// const { equal } = require('assert');
 
 // these pass
-equal({a: "1"}, {a: 1}, 'meh')
-equal(1, "1")
+// equal({a: "1"}, {a: 1}, 'meh')
+// equal(1, "1")
 
 
 
 
 
-// deepStrictEqual({a: "1", b: {c: "1"}}, {a: "1", b: {c: 1}}, 'meh')
+const { strictEqual } = require('assert');
+
+// these fail
+// strictEqual({a: "1"}, {a: 1}, 'meh')
+// strictEqual(1, "1")
+
+// these pass
+const a = {a: "1"}
+strictEqual(a, a, 'meh')
+strictEqual(1, 1, 'same number')
