1eeab2f05b3ed0ac293c31eed5b71a65f2f4a76d
Eric Laursen <ericjlaursen@gmail.com>
2021-03-26 20:07:45 -0400

    init

---
+++ in-depth/child-process/fork/about.md
@@ -0,0 +1,1 @@
+## Forking for http server performance
---
+++ in-depth/child-process/fork/blocking-server.js
@@ -0,0 +1,72 @@
+//Dependency
+const http = require('http');
+const port = 3000;
+const urlNode = require('url')
+
+const server = http.createServer(({url, method, query}, res) => {
+
+	//get & parse the url
+	const parsedUrl = urlNode.parse(url, true);
+
+	//get the 'path' name from the url, trim the pathText
+	const pathText = parsedUrl.pathname;
+	const trimmedPathTxt = pathText.replace(/^\/+|\/+$/g,'')
+  
+	//get http method that was used. its in the req object
+	var reqMethod = method.toLowerCase()
+
+	//'log' the request path
+	console.log(`request recieved on ${trimmedPathTxt || '/'} with method ${reqMethod}`)
+  if(trimmedPathTxt !== 'favicon.ico'){
+    // send the response
+	  res.end('basic node server response string\n');
+  }else{
+    res.end('favicon')
+  }
+
+})
+
+//Start the server, listen on port 3000
+server.listen(port, () => console.log(`Server is listening on port ${port}!!`))
+
+// app.get("/isprime", (req, res) => {
+//     res.setHeader("Access-Control-Allow-Origin","*")
+//     const jsonResponse = isPrime(parseInt(req.query.number))
+//     res.send(jsonResponse)
+// } )
+
+// app.listen(8081, ()=>console.log("Listening on 8081") )
+
+// function isPrime(number) {
+//     let startTime = new Date();
+//     let endTime = new Date();
+//     let isPrime = true;
+//     for (let i = 3; i < number; i ++)
+//     {   
+//         //it is not a prime break the loop,
+//         // see how long it took
+//         if (number % i === 0) 
+//         {
+//             endTime = new Date();
+//             isPrime = false;
+//             break;
+//         }
+//     }
+
+//     if (isPrime) endTime = new Date();
+
+//     return {
+//         "number" : number,
+//         "isPrime": isPrime,
+//         "time": endTime.getTime() - startTime.getTime()
+//         }
+
+// }
+
+/*
+2367949 (16 ms)
+43686389 (200 ms)
+93686687 (500 ms)
+936868033(4 seconds)
+29355126551 (very long time)
+*/
