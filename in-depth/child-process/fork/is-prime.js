dcf5ba2941faedc47c1db52fd6a6e192e3af0bc4
Eric Laursen <ericjlaursen@gmail.com>
2021-03-26 20:54:20 -0400

    an is-prime example

---
+++ in-depth/child-process/fork/is-prime.js
@@ -0,0 +1,26 @@
+module.exports =  function isPrime(number, pid) {
+    let startTime = new Date();
+    let endTime = new Date();
+    let isPrime = true;
+    for (let i = 3; i < number; i ++)
+    {   
+        //it is not a prime break the loop,
+        // see how long it took
+        if (number % i === 0) 
+        {
+            endTime = new Date();
+            isPrime = false;
+            break;
+        }
+    }
+
+    if (isPrime) endTime = new Date();
+
+    return {
+        "number" : number,
+        "isPrime": isPrime,
+        "time": endTime.getTime() - startTime.getTime(),
+        pid
+        }
+
+}
