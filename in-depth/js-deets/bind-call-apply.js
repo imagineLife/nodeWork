f91baac22281b17f831736a23f083804d887a2ca
Eric Laursen <ericjlaursen@gmail.com>
2021-06-17 21:02:40 -0400

    bind init

--- in-depth/js-deets/bind-call-apply.js
+++ in-depth/js-deets/bind-call-apply.js
@@ -10,6 +10,10 @@ let o = {
   num: 2
 }
 
+let bigO = {
+  num: 14
+}
+
 /*
   CALL
   functionName.call(focusObj, fnParams)
@@ -19,14 +23,26 @@ let o = {
 let newNum = addToThis.call(o,4);
 console.log({newNum})
 
+// params can be added as a list
 let biggerNum = addAFew.call(o,2,3,4)
 console.log({biggerNum})
 
+/*
+  APPLY!
+  like call but can take an arr
+*/
 let argsArr = [3,4,5]
 let bigAgain = addAFew.apply(o,argsArr);
 console.log({bigAgain})
 
+let biggerArgsArr = [4,5,6]
+let evenBig = addAFew.apply(bigO,biggerArgsArr);
+console.log({evenBig})
 
 
-
-
+/*
+  BIND
+  does NOT give the result when logged result
+*/ 
+let bindOne = addToThis.bind(o,4);
+console.log({bindOne})
