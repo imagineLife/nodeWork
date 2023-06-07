90a7ea30d7a3955b54def1582fae049a1b4cf8e6
Eric Laursen <ericjlaursen@gmail.com>
2023-06-06 21:07:36 -0400

    move session handlers into sub-directory

--- in-depth/http-server-bits/with-session/index.js
+++ in-depth/http-server-bits/with-session/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cookieSession = require('cookie-session');
+const sessionsRouter = require('./session-route-handler')
 const PORT = process.env.PORT || 3000;
 const SESSION_NAME = 'test-session';
 const SESSION_SECRET = process.env.SESSION_SECRET || 'generic-secret';
@@ -11,40 +12,17 @@ const sessionObj = {
 }
 const app = express();
 
+function startCallback() {
+  console.log(`Server started on port ${PORT}`);  
+}
+
 // Middleware to parse request bodies
 app.use(express.json());
 
 // Configure cookie-session middleware
 app.use(cookieSession(sessionObj));
 
-// Endpoint to create the session
-app.get('/make-a-session', (req, res) => {
-  return res.json(req.session);
-});
-
-app.get('/read-the-session', (req, res) => {
-  return res.json(req.session)
-});
-
-app.get('/update-session/:randomString', (req, res) => {
-  req.session.randomString = req.params.randomString
-  return res.json(req?.session);
-});
-
-app.get('/keyVal/:myKey/:myVal', (req, res) => {
-  console.log('req.params.myKey')
-  console.log(req.params.myKey)
-  
-  req.session[`${req.params.myKey}`] = `${req.params.myVal}`;
-  return res.json(req?.session);
-});
-
-app.get('/clear-session', (req, res) => {
-  req.session = null;
-  return res.json(req?.session)
-});
+app.use('/mySession', sessionsRouter);
 
 // Start the server
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
-});
+app.listen(PORT, startCallback);
---
+++ in-depth/http-server-bits/with-session/session-route-handler.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const sessionsRouter = express.Router();
+
+// read the session
+sessionsRouter.get('/', (req, res) => {
+  return res.json(req.session);
+});
+
+// 
+sessionsRouter.get('/update/:randomString', (req, res) => {
+  req.session.randomString = req.params.randomString
+  return res.json(req?.session);
+});
+
+sessionsRouter.get('/keyVal/:myKey/:myVal', (req, res) => {
+  req.session[`${req.params.myKey}`] = `${req.params.myVal}`;
+  return res.json(req?.session);
+});
+
+sessionsRouter.get('/clear', (req, res) => {
+  req.session = null;
+  return res.json(req?.session)
+});
+
+// sessionsRouter.get('/justNext', (req, res, next) => {
+//   next()
+// });
+module.exports = sessionsRouter;
