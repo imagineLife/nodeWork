45152e4474c4fb9120a0092956899086ebc52d25
Eric Laursen <ericjlaursen@gmail.com>
2023-09-23 11:01:46 -0400

    .

--- in-depth/child-process/withExec/countLength.js
+++
@@ -1,9 +0,0 @@
-// console.log({
-//   countLength: 1,
-//   idx: process.env.idx,
-//   sentence: process.env.text
-// })
-
-// const length = process.env.text.length()
-
-console.log(JSON.stringify({ strLength: process.env.text.length, pid: process.pid }));
--- in-depth/child-process/withExec/logastring.js
+++
@@ -1,5 +0,0 @@
-// console.log({
-//   idx: process.env.idx,
-//   sentence: process.env.text
-// })
-console.log(JSON.stringify({ splitCount: process.env.text.split(' ').length, pid: process.pid }));
